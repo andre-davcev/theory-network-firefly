c668ae6006a071c50f73a766489af6f957ed54a0
Andre Davcev <andre.davcev@gmail.com>
2016-04-26 21:28:45 -0400

    Intermediate commit

--- typescript/index.d.ts
+++
@@ -1,7 +0,0 @@
-export {TNObject} from './base/theory.base.object';
-
-export {TNFirebaseUtility} from './firebase/theory.firebase.utility';
-
-export {IonicConfiguration} from './ionic/theory.ionic.configuration';
-
-export {TNConfiguration} from './utility/theory.utility.configuration';
