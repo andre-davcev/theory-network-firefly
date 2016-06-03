75ffc622056a0c8f44701f00341ebbe49279a7ff
Andre Davcev <andre.davcev@gmail.com>
2016-06-03 19:53:53 -0400

    Provider icon styles

--- typescript/directives/theory.directive.input.password.ts
+++ typescript/directives/theory.directive.input.password.ts
@@ -11,7 +11,7 @@ import {TNInput}  from './theory.directive.input';
     <form class="tn-input-container tn-password {{hostClasses}}" name="form" [class.tn-input-status-verified]="isVerified()" [class.tn-input-status-error]="isError()" [class.tn-input-status-rounded]="roundedIcons">
         <input class="tn-input" type="password" name="input" ngControl="valueControl" [(ngModel)]="value" placeholder="{{placeholder}}" required tn-trim="trim" tn-pattern="pattern">
 
-        <ion-spinner icon="spiral" class="spinner spinner-spiral {{spinnerClasses}}" [hidden]="!verifying"></ion-spinner>
+        <ion-spinner icon="spiral" [hidden]="!verifying"></ion-spinner>
         <div class="tn-input-clear" [hidden]="!(clear && showClear)" (click)="clearValue()"></div>
     </form>
     `
--- typescript/directives/theory.directive.input.text.ts
+++ typescript/directives/theory.directive.input.text.ts
@@ -11,7 +11,7 @@ import {TNInput}  from './theory.directive.input';
     <form class="tn-input-container tn-text {{hostClasses}}" name="form" [class.tn-input-status-verified]="isVerified()" [class.tn-input-status-error]="isError()" [class.tn-input-status-rounded]="roundedIcons">
         <input class="tn-input" type="text" name="input" ngControl="valueControl" [(ngModel)]="value" placeholder="{{placeholder}}" required tn-trim="trim" tn-pattern="pattern">
 
-        <ion-spinner icon="spiral" class="spinner spinner-spiral {{spinnerClasses}}" [hidden]="!verifying"></ion-spinner>
+        <ion-spinner icon="spiral" [hidden]="!verifying"></ion-spinner>
         <div class="tn-input-clear" [hidden]="!(clear && showClear)" (click)="clearValue()"></div>
     </form>
     `
