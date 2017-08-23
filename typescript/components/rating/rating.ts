27dbd8954193c631936aa78a96f2e9432ebe2dbd
Andre Davcev <andre.davcev@gmail.com>
2017-08-22 22:40:14 -0400

    Intermediate commit for <tn-rating>

--- typescript/components/rating/rating.ts
+++ typescript/components/rating/rating.ts
@@ -1,5 +1,4 @@
 import {Input}         from '@angular/core';
-import {OnInit}        from '@angular/core';
 import {OnChanges}     from '@angular/core';
 import {SimpleChanges} from '@angular/core';
 import {Component}     from '@angular/core';
@@ -19,37 +18,55 @@ import {TNComponent} from '../component';
     
     template :
     `
-    <ion-icon name="star-outline"></ion-icon>
-    <ion-icon name="star"></ion-icon>
-    <ion-icon name="star-half"></ion-icon>
+    <ion-icon class="tn-star" name="star" *ngIf="stars[0]"></ion-icon>
+    <ion-icon class="tn-star-half" name="star-half" *ngIf="halfs[0]"></ion-icon>
+    <ion-icon class="tn-star-outline" name="star-outline"></ion-icon>
 
-    <ion-icon name="star-outline"></ion-icon>
-    <ion-icon name="star"></ion-icon>
-    <ion-icon name="star-half"></ion-icon>
+    <ion-icon class="tn-star" name="star" *ngIf="stars[1]"></ion-icon>
+    <ion-icon class="tn-star-half" name="star-half" *ngIf="halfs[1]"></ion-icon>
+    <ion-icon class="tn-star-outline" name="star-outline"></ion-icon>
 
-    <ion-icon name="star-outline"></ion-icon>
-    <ion-icon name="star"></ion-icon>
-    <ion-icon name="star-half"></ion-icon>
+    <ion-icon class="tn-star" name="star" *ngIf="stars[2]"></ion-icon>
+    <ion-icon class="tn-star-half" name="star-half" *ngIf="halfs[2]"></ion-icon>
+    <ion-icon class="tn-star-outline" name="star-outline"></ion-icon>
 
-    <ion-icon name="star-outline"></ion-icon>
-    <ion-icon name="star"></ion-icon>
-    <ion-icon name="star-half"></ion-icon>
+    <ion-icon class="tn-star" name="star" *ngIf="stars[3]"></ion-icon>
+    <ion-icon class="tn-star-half" name="star-half" *ngIf="halfs[3]"></ion-icon>
+    <ion-icon class="tn-star-outline" name="star-outline"></ion-icon>
 
-    <ion-icon name="star-outline"></ion-icon>
-    <ion-icon name="star"></ion-icon>
-    <ion-icon name="star-half"></ion-icon>
+    <ion-icon class="tn-star" name="star" *ngIf="stars[4]"></ion-icon>
+    <ion-icon class="tn-star-half" name="star-half" *ngIf="halfs[4]"></ion-icon>
+    <ion-icon class="tn-star-outline" name="star-outline"></ion-icon>
 
     <div>{{ratingCount}}</div>
     `
 })
 
-export class TNRating extends TNComponent implements OnInit, OnChanges
+export class TNRating extends TNComponent implements OnChanges
 {
     @Input()
     protected rating:number;
 
     @Input()
     protected ratingCount:number;
+
+    protected stars:Array<boolean> =
+    [
+        false,
+        false,
+        false,
+        false,
+        false
+    ];
+
+    protected halfs:Array<boolean> =
+    [
+        false,
+        false,
+        false,
+        false,
+        false
+    ];
 
     constructor(elementRef:ElementRef, renderer: Renderer)
     {
@@ -58,11 +75,6 @@ export class TNRating extends TNComponent implements OnInit, OnChanges
             elementRef : elementRef,
             renderer   : renderer
         });
-    }
-
-    ngOnInit()
-    {
-        
     }
 
     ngOnChanges(changes: SimpleChanges)
@@ -75,6 +87,28 @@ export class TNRating extends TNComponent implements OnInit, OnChanges
 
     protected changeRating(rating:number)
     {
+        let stars:Array<boolean> = this.stars;
+        let halfs:Array<boolean> = this.halfs;
 
+        if (rating < 0)
+        {
+            rating = 0;
+        }
+        else if (rating > 5)
+        {
+            rating = 5;
+        }
+
+        for (let i = 0; i < 5; i++)
+        {
+            if (rating >= (i + 1))
+            {
+                stars[i] = true;
+            }
+            else if (rating >= i + 0.5)
+            {
+                halfs[i] = true;
+            }
+        }
     }
 }
