36c49e69224c7108890880f3ed4007fcf5746b82
Andre Davcev <andre.davcev@gmail.com>
2017-11-03 20:27:54 -0400

    Intermediate commit

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+'use strict';
+
+module.exports = function(grunt)
+{
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig(
+    {
+        package : grunt.file.readJSON('package.json'),
+
+        module :
+        {
+            name   : 'theory-network',
+            path   : '.',
+            source : '<%= module.path %>/typescript',
+            build  : '../app/modules/<%= module.name %>'
+        },
+
+        clean :
+        {
+            options : {force : true},
+
+            build :
+            {
+                src :
+                [
+                    '<%= module.build %>'
+                ]
+            },
+
+            typescript :
+            {
+                src :
+                [
+                    '<%= module.path %>/.tscache',
+                    '<%= module.path %>/tscommand-*',
+                    '<%= module.path %>/.baseDir.*'
+                ]
+            }
+        },
+
+        copy :
+        {
+            package :
+            {
+                files :
+                [
+                    {expand : true, cwd : '<%= module.source %>', src : 'package.json', dest : '<%= module.build %>'}
+                ]
+            }
+        },                 
+
+        watch :
+        {
+            module :
+            {
+                files :
+                [
+                    '<%= module.source %>/*.ts',
+                    '<%= module.source %>/**/*.ts'
+                ],
+
+                tasks :
+                [
+                    'build'
+                ]
+            }
+        },
+
+        ts :
+        {
+            library :
+            {
+                tsconfig : {},
+
+                src :
+                [
+                    '<%= module.source %>/*.ts'
+                ],
+
+                dest : '<%= module.build %>'
+            }
+        },
+
+        replace :
+        {
+            module :
+            {
+                src  : ['<%= module.build %>/index.d.ts'],
+                dest : '<%= module.build %>/index.d.ts',
+
+                replacements :
+                [
+                    {
+                        from : '/// <reference path="../../../library/node_modules/typescript/lib/lib.es6.d.ts" />',
+                        to   : ''
+                    }
+                ]
+            }
+        }
+    });
+
+    //////////////////////////////////////////////////////////////
+    //  Shared tasks between projects
+    //////////////////////////////////////////////////////////////
+    grunt.registerTask('build',
+    [
+        'clean:build',
+        'ts:library',
+        'clean:typescript',
+        'copy:package',
+        'replace:module'
+    ]);
+
+    grunt.registerTask('build-watch',
+    [
+        'build',
+        'watch:module'
+    ]);
+
+    //////////////////////////////////////////////////////////////
+
+};
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,45 @@
-export * from './module';
+///<reference path="../node_modules/typescript/lib/lib.es6.d.ts"/>
+
+import {NgModule}            from '@angular/core';
+import {ModuleWithProviders} from '@angular/core';
+
+export * from './base/module';
+export * from './components/module';
+export * from './config/module';
+export * from './directives/module';
+export * from './ionic/module';
+export * from './utility/module';
+
+import {TNBaseModule}       from './base/module';
+import {TNComponentModule}  from './components/module';
+import {TNConfigModule}     from './config/module';
+import {TNDirectivesModule} from './directives/module';
+import {TNIonicModule}      from './ionic/module';
+import {TNUtilModule}       from './utility/module';
+
+@NgModule
+({
+    imports :
+    [
+        TNBaseModule,
+        TNComponentModule,
+        TNConfigModule,
+        TNDirectivesModule,
+        TNIonicModule,
+        TNUtilModule
+    ],
+
+    declarations : [],
+    exports      : []
+})
+
+export class TheoryNetworkModule
+{
+    static forRoot():ModuleWithProviders
+    {
+        return {
+            ngModule  : TheoryNetworkModule,
+            providers : []
+        }
+    }
+}
