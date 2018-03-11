6df8349f2e6404c3b1d43218e9bb873685fc9d66
Andre Davcev <andre.davcev@gmail.com>
2018-03-11 14:19:10 -0400

    Intermediate commit

--- typescript/components/component.app.ts
+++ typescript/components/component.app.ts
@@ -1,13 +1,36 @@
-import {OnDestroy}    from '@angular/core';
+import {OnDestroy}     from '@angular/core';
+import {OnChanges}     from '@angular/core';
+import {SimpleChanges} from '@angular/core';
+import {Input}         from '@angular/core';
+import {HostBinding}   from '@angular/core';
+
 import {Subscription} from 'rxjs/Subscription';
 
-export class AppComponent implements OnDestroy
+export class AppComponent implements OnChanges, OnDestroy
 {
+    @HostBinding('class')
+    @Input()
+    public class: string;
+  
+    private _classPrefix = 'app-';
+    public classes: { [id: string]: string } = {};
+
     private _subscriptions: Array<Subscription> = [];
 
     constructor()
     {
 
+    }
+
+    ngOnChanges(changes: SimpleChanges): void
+    {
+        if (changes.class)
+        {
+            if (changes.class.firstChange && changes.class.currentValue != null)
+            {
+                this.classesAdd(changes.class.currentValue.trim().split(' '));
+            }
+        }
     }
 
     ngOnDestroy()
@@ -21,5 +44,48 @@ export class AppComponent implements OnDestroy
     public subscriptionsAdd(...subscriptions: Array<Subscription>)
     {
         this._subscriptions.concat(subscriptions);
+    }
+
+    public getClass(className: string)
+    {
+        return `${this._classPrefix}${className}`;
+    }
+
+    public classesAdd(...classes: Array<string>): void {
+        for (const elementClass of classes) {
+          if (elementClass != null) {
+            this.classes[elementClass] = '';
+          }
+        }
+    
+        this.classesRender();
+    }
+    
+    public classesRemove(...classes: Array<string>): void
+    {
+        for (const elementClass of classes)
+        {
+            if (elementClass != null)
+            {
+                delete this.classes[elementClass];
+            }
+        }
+    
+        this.classesRender();
+    }
+    
+    private classesRender()
+    {
+        this.class = Object.keys(this.classes).join(' ');
+    }
+    
+    get classPrefix(): string
+    {
+        return this._classPrefix;
+    }
+    
+    set classPrefix(classPrefix: string)
+    {
+        this._classPrefix = classPrefix;
     }
 }
