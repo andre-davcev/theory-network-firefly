4bb89210a23cc0bca41c77ab2e42c998a9ab0644
Andre Davcev <andre.davcev@gmail.com>
2018-04-17 23:57:18 -0400

    Initial migration to nrwl

--- ts/directives/version/version.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Version
-{
-    major           : number;
-    minor           : number;
-    patch           : number;
-    feature?        : string;
-    featureVersion? : number;
-}
