ff40eeaf330da8852b8b8315a24e21e2f662b1bb
Andre Davcev <andre.davcev@gmail.com>
2018-04-18 00:23:44 -0400

    Added nrwl generated @firefly/components library

---
+++ libs/firefly/components/index.ts
@@ -0,0 +1,1 @@
+export { ComponentsModule } from './src/components.module';
---
+++ libs/firefly/components/src/components.module.spec.ts
@@ -0,0 +1,7 @@
+import { ComponentsModule } from './components.module';
+
+describe('ComponentsModule', () => {
+  it('should work', () => {
+    expect(new ComponentsModule()).toBeDefined();
+  });
+});
