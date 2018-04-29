d9a49de810a1e9d3b1e013c4a8f6558373afd790
Andre Davcev <andre.davcev@gmail.com>
2018-04-29 14:23:44 -0400

    FIR-1066: Added cluster categories page

---
+++ apps/firefly/mobile/src/pages/cluster-categories/cluster-categories.page.ts
@@ -0,0 +1,25 @@
+import {Component, AfterViewInit} from '@angular/core';
+
+import {IonicPage} from 'ionic-angular';
+
+import { FormGroup } from '@angular/forms';
+import { StateCluster, SetClusterId } from '../../ngxs/cluster/cluster.state';
+import { Observable } from 'rxjs/Observable';
+import { Select, Store } from '@ngxs/store';
+
+@IonicPage()
+@Component
+({
+    selector    : 'app-page-cluster-categories',
+    templateUrl : 'cluster-categories.page.html'
+})
+
+export class PagePublisherClusterCategories
+{
+    @Select(StateCluster.form) form$: Observable<FormGroup>;
+
+    constructor(private store: Store)
+    {
+
+    }
+}
