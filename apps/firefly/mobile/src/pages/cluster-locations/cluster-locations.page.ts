d49302c5ffb66315085bfe67959f0cd4486a0605
Andre Davcev <andre.davcev@gmail.com>
2018-04-29 15:52:59 -0400

    FIR-1239: Leaflet install

--- apps/firefly/mobile/src/pages/cluster-locations/cluster-locations.page.ts
+++ apps/firefly/mobile/src/pages/cluster-locations/cluster-locations.page.ts
@@ -7,6 +7,8 @@ import { StateCluster } from '../../ngxs/cluster/cluster.state';
 import { Observable } from 'rxjs/Observable';
 import { Select, Store } from '@ngxs/store';
 
+import { MapOptions, tileLayer, latLng } from 'leaflet';
+
 @IonicPage()
 @Component
 ({
@@ -17,6 +19,17 @@ import { Select, Store } from '@ngxs/store';
 export class PagePublisherClusterLocations
 {
     @Select(StateCluster.form) form$: Observable<FormGroup>;
+
+    public options: MapOptions =
+    {
+        layers :
+        [
+            tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+        ],
+
+        zoom: 5,
+        center: latLng(46.879966, -121.726909)
+    };
 
     constructor(private store: Store)
     {
