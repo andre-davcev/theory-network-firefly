6f9c69306aa597e927a15c0685b097e63fe05375
Andre Davcev <andre.davcev@gmail.com>
2018-05-30 21:08:33 -0400

    Basic page structures for assets set up

---
+++ apps/firefly/mobile/src/pages/assets-events/assets-events.page.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+import {IonicPage} from 'ionic-angular';
+
+@IonicPage()
+@Component
+({
+    selector    : 'app-page-assets-events',
+    templateUrl : 'assets-events.page.html'
+})
+
+export class PageAssetsEvents
+{
+    constructor()
+    {
+
+    }
+}
