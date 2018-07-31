19802d93fd13e334992b7946245373013978c560
Andre Davcev <andre.davcev@gmail.com>
2018-07-31 13:23:52 -0400

    Adding firefly mobile app "ng g app.ionic firefly"

---
+++ apps/ionic-firefly/src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+// libs
+import { TnIonicCoreModule } from '@theory/ionic';
+
+@NgModule({
+  imports: [TnIonicCoreModule]
+})
+export class CoreModule {}
