7edb7fed5ad5e91bfb54457692cf209191a9c8ba
Andre Davcev <andre.davcev@gmail.com>
2018-08-13 18:32:26 -0400

    Adding back environment variables

--- apps/firefly/mobile/src/environments/environment.prod.ts
+++ apps/firefly/mobile/src/environments/environment.prod.ts
@@ -1,3 +1,34 @@
-export const environment = {
-  production: true
+import { Environment } from './environment.model';
+
+export const environment: Environment =
+{
+    production : true,
+    language   : 'en',
+    version    : '0.0.0',
+    pathJson   : 'data',
+
+    apis :
+    {
+        firebase :
+        {
+            apiKey            : '***REMOVED-FIREBASE-API-KEY***',
+            authDomain        : 'firefly-b1072.firebaseapp.com',
+            databaseURL       : 'https://firefly-b1072.firebaseio.com',
+            projectId         : 'firefly-b1072',
+            storageBucket     : 'firefly-b1072.appspot.com',
+            messagingSenderId : '827673030572'
+        },
+
+        places :
+        {
+            clientId     : '0OTN4VI0TZMQGRTJPWOUVDYUJK0VHH3YCDRQO0CAEUKG43FI',
+            clientSecret : 'B0OICO4SNQ1EXRKWZSZ0JN0K3QXZCSUBP3PS1MN1NFC4YPQD',
+            url          : 'https://api.foursquare.com/v2/venues'
+        },
+
+        maps :
+        {
+            accessToken: '***REMOVED-MAPBOX-TOKEN***'
+        }
+    }
 };
--- apps/firefly/mobile/src/environments/environment.ts
+++ apps/firefly/mobile/src/environments/environment.ts
@@ -1,15 +1,34 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-export const environment = {
-  production: false
-};
+import { Environment } from './environment.model';
 
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+export const environment: Environment =
+{
+    production : false,
+    language   : 'en',
+    version    : '0.0.0',
+    pathJson   : 'data',
+
+    apis :
+    {
+        firebase :
+        {
+            apiKey            : '***REMOVED-FIREBASE-API-KEY***',
+            authDomain        : '1388625286411.firebaseapp.com',
+            databaseURL       : 'https://1388625286411.firebaseio.com',
+            projectId         : 'project-4334231676697990915',
+            storageBucket     : 'project-4334231676697990915.appspot.com',
+            messagingSenderId : '671375922961'
+        },
+
+        places :
+        {
+            clientId     : '0OTN4VI0TZMQGRTJPWOUVDYUJK0VHH3YCDRQO0CAEUKG43FI',
+            clientSecret : 'B0OICO4SNQ1EXRKWZSZ0JN0K3QXZCSUBP3PS1MN1NFC4YPQD',
+            url          : 'https://api.foursquare.com/v2/venues'
+        },
+
+        maps :
+        {
+            accessToken: '***REMOVED-MAPBOX-TOKEN***'
+        }
+    }
+};
