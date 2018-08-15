fb644334b561dfb3b0c49610d6d6ba366fda9972
Andre Davcev <andre.davcev@gmail.com>
2018-08-14 21:47:18 -0400

    Re-adding ionic firefly app

---
+++ apps/ionic-firefly/src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+// libs
+import { AppIonicCoreModule } from '@theory/ionic';
+
+@NgModule({
+  imports: [AppIonicCoreModule]
+})
+export class CoreModule {}
