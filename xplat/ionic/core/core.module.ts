d68b73fd03a4d4af26c94c9e4ab8b4cf5a178a02
Andre Davcev <andre.davcev@gmail.com>
2018-08-14 21:43:34 -0400

    Adding xplat back to project "ng g xplat --prefix=app --platforms=web,nativescript,ionic"

---
+++ xplat/ionic/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+
+import { IonicModule } from '@ionic/angular';
+import { throwIfAlreadyLoaded } from '@theory/utils';
+import { AppCoreModule } from '@theory/web';
+
+@NgModule({
+  imports: [AppCoreModule, IonicModule.forRoot()]
+})
+export class AppIonicCoreModule {
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: AppIonicCoreModule
+  ) {
+    throwIfAlreadyLoaded(parentModule, 'AppIonicCoreModule');
+  }
+}
