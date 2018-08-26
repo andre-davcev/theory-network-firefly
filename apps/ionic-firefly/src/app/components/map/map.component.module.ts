c165655ebf1af8d9736050425e6673df5e78901b
Andre Davcev <andre.davcev@gmail.com>
2018-08-26 14:35:11 -0400

    Fixing map component imports

--- apps/ionic-firefly/src/app/components/map/map.component.module.ts
+++ apps/ionic-firefly/src/app/components/map/map.component.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { ComponentMap } from './map.component';
-import { ModuleMapOrb } from '../map-orb/map-orb.module';
+import { ModuleMapOrb } from '../map-orb/map-orb.component.module';
 
 @NgModule
 ({
