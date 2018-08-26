a5f5e026e09c57c8251a968c4f2482da1b10392a
Andre Davcev <andre.davcev@gmail.com>
2018-08-26 13:42:31 -0400

    Moved firebase modules from core to shared module

--- apps/ionic-firefly/src/app/core/core.module.ts
+++ apps/ionic-firefly/src/app/core/core.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 
 // libs
 import { AppIonicCoreModule } from '@theory/ionic';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   imports: [
-    AppIonicCoreModule,
-    AngularFireAuthModule,
-    AngularFirestoreModule
+    AppIonicCoreModule
   ]
 })
 export class CoreModule {}
