b9ed5b00bce05613a98b42f0c4643e916069b92c
Andre Davcev <andre.davcev@gmail.com>
2018-08-26 13:04:11 -0400

    Moved old libs to ionic-firefly

--- apps/ionic-firefly/libs.old/core/classes/index.ts
+++
@@ -1,6 +0,0 @@
-export { ClassesModule } from './src/classes.module';
-
-export { ComponentApp }     from './src/component/component-app.class';
-export { ComponentOptions } from './src/component/component-options.interface';
-export { Component }        from './src/component/component.class';
-export { Icon }             from './src/component/icon.class';
