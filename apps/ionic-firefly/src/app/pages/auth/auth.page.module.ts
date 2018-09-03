cc361f196396e8d4434f00cef66f68bbbec941ac
Andre Davcev <andre.davcev@gmail.com>
2018-09-02 23:59:00 -0400

    Sass variables & global work

--- apps/ionic-firefly/src/app/pages/auth/auth.page.module.ts
+++ apps/ionic-firefly/src/app/pages/auth/auth.page.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { PageLogin } from './auth.page';
 import { ModulePage } from '../page.module';
+import { ModuleComponentIconFirefly } from '../../components/icon-firefly/icon-firefly.component.module';
 
 @NgModule
 ({
     imports :
     [
-        ModulePage
+        ModulePage,
+        ModuleComponentIconFirefly
     ],
 
     declarations : [PageLogin]
