91209e7f7f9fcd45aaca71f8340ecb1e576a4e83
Andre Davcev <andre.davcev@gmail.com>
2018-09-16 21:38:43 -0400

    Removed facebook & background geolocation plugins. Also changing home styles.

--- apps/ionic-firefly/android/app/src/main/assets/public/cordova_plugins.js
+++ apps/ionic-firefly/android/app/src/main/assets/public/cordova_plugins.js
@@ -2,14 +2,6 @@
   cordova.define('cordova/plugin_list', function(require, exports, module) {
     module.exports = [
       {
-        "id": "cordova-plugin-facebook4.FacebookConnectPlugin",
-        "file": "plugins/cordova-plugin-facebook4/www/facebook-native.js",
-        "pluginId": "cordova-plugin-facebook4",
-        "clobbers": [
-          "facebookConnectPlugin"
-        ]
-      },
-      {
         "id": "cordova-plugin-globalization.GlobalizationError",
         "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
         "pluginId": "cordova-plugin-globalization",
@@ -24,27 +16,12 @@
         "clobbers": [
           "navigator.globalization"
         ]
-      },
-      {
-        "id": "cordova-plugin-mauron85-background-geolocation.BackgroundGeolocation",
-        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/BackgroundGeolocation.js",
-        "pluginId": "cordova-plugin-mauron85-background-geolocation",
-        "clobbers": [
-          "BackgroundGeolocation"
-        ]
-      },
-      {
-        "id": "cordova-plugin-mauron85-background-geolocation.radio",
-        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/radio.js",
-        "pluginId": "cordova-plugin-mauron85-background-geolocation"
       }
     ];
     module.exports.metadata =
     // TOP OF METADATA
     {
-      "cordova-plugin-facebook4": "2.4.0",
-      "cordova-plugin-globalization": "1.11.0",
-      "cordova-plugin-mauron85-background-geolocation": "3.0.0-alpha.41"
+      "cordova-plugin-globalization": "1.11.0"
     };
     // BOTTOM OF METADATA
     });
