350044f50e141be3153cb6f17b88c221fb6a5581
Andre Davcev <>
2018-10-03 23:32:41 -0400

    Added Page class to unsubscribe from subscriptions on component destroy

---
+++ apps/ionic-firefly/src/app/classes/page.class.ts
@@ -0,0 +1,17 @@
+import { OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+export class Page implements OnDestroy
+{
+    private subscriptions: Array<Subscription>;
+
+    protected subscriptionsAdd(...subscriptions: Array<Subscription>): void
+    {
+        this.subscriptions.concat(subscriptions);
+    }
+
+    ngOnDestroy(): void
+    {
+        this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
+    }
+}
