f666783d6b0a75e0f9dfed248c7b2c57fc0214bd
Andre Davcev <>
2018-10-13 10:48:01 -0400

    Re-adding firefly-mobile as publishable

---
+++ libs/firefly-mobile/src/lib/firefly-mobile.module.spec.ts
@@ -0,0 +1,14 @@
+import { async, TestBed } from '@angular/core/testing';
+import { FireflyMobileModule } from './firefly-mobile.module';
+
+describe('FireflyMobileModule', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [FireflyMobileModule]
+    }).compileComponents();
+  }));
+
+  it('should create', () => {
+    expect(FireflyMobileModule).toBeDefined();
+  });
+});
