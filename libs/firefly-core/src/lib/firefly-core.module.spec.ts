8067e0ea425f55d82e469b0f51cb3d4ed2a1343d
Andre Davcev <>
2018-10-12 22:08:27 -0400

    Added @firefly/core

---
+++ libs/firefly-core/src/lib/firefly-core.module.spec.ts
@@ -0,0 +1,14 @@
+import { async, TestBed } from '@angular/core/testing';
+import { FireflyCoreModule } from './firefly-core.module';
+
+describe('FireflyCoreModule', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [FireflyCoreModule]
+    }).compileComponents();
+  }));
+
+  it('should create', () => {
+    expect(FireflyCoreModule).toBeDefined();
+  });
+});
