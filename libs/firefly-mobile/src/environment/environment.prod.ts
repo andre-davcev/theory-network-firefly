43d058086affde0bf9375315126d098eab303858
Andre Davcev <>
2018-10-13 10:52:01 -0400

    Re-adding back enviroment folder to firefly-mobile

---
+++ libs/firefly-mobile/src/environment/environment.prod.ts
@@ -0,0 +1,35 @@
+import { Environment } from '@firefly/mobile';
+
+export const environmentProd: Environment =
+{
+    production  : true,
+    environment : 'prod',
+    language    : 'en',
+    version     : '0.0.0',
+    pathJson    : 'data',
+
+    apis :
+    {
+        firebase :
+        {
+            apiKey            : 'AIzaSyBYE4fzg9My7F8W_kLpoSb58NaPvhWhWRc',
+            authDomain        : 'firefly-b1072.firebaseapp.com',
+            databaseURL       : 'https://firefly-b1072.firebaseio.com',
+            projectId         : 'firefly-b1072',
+            storageBucket     : 'firefly-b1072.appspot.com',
+            messagingSenderId : '827673030572'
+        },
+
+        places :
+        {
+            clientId     : '0OTN4VI0TZMQGRTJPWOUVDYUJK0VHH3YCDRQO0CAEUKG43FI',
+            clientSecret : 'B0OICO4SNQ1EXRKWZSZ0JN0K3QXZCSUBP3PS1MN1NFC4YPQD',
+            url          : 'https://api.foursquare.com/v2/venues'
+        },
+
+        maps :
+        {
+            accessToken: 'pk.eyJ1IjoidGhlb3J5bmV0d29yayIsImEiOiJjamdwem04eGwwMXVsMnZwaGR2YzZxdGxvIn0.1mwIacOT0bTANo6lueSQmg'
+        }
+    }
+};
