1c94924d7534648195fb7211afa3f7fa8970824f
Andre Davcev <>
2018-10-16 19:01:25 -0400

    Libraries now install via npm and resolve via paths!!!

--- libs/web/src/features/ui/ui.module.ts
+++ libs/web/src/features/ui/ui.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
-// libs
-import { UISharedModule } from '@theory/features';
+import { TranslateModule } from '@ngx-translate/core';
 
 const MODULES = [
   CommonModule,
   RouterModule,
   FormsModule,
   ReactiveFormsModule,
-  UISharedModule
+  TranslateModule
 ];
 
 @NgModule({
