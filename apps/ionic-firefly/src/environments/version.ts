5c45272689b90e7e022804c9e31d259128281c7f
Andre Davcev <>
2018-10-20 23:53:34 -0400

    Created script to generate version.ts file based off package.json version number on postinstall of "npm i". See: https://medium.com/@amcdnl/version-stamping-your-app-with-the-angular-cli-d563284bb94d

---
+++ apps/ionic-firefly/src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "1a91259-dirty",
+    "hash": "1a91259",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "1a91259-dirty",
+    "semverString": null,
+    "version": "1.0.0-alpha.0"
+};
+/* tslint:enable */
