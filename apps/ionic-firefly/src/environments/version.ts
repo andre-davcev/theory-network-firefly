6fcb49d93fd9f8ede97d5f46ba1b753c4082b195
Andre Davcev <>
2018-11-04 23:04:56 -0500

    Making sure app is running again back on iOS

--- apps/ionic-firefly/src/environments/version.ts
+++ apps/ionic-firefly/src/environments/version.ts
@@ -2,12 +2,12 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "bbf4578-dirty",
-    "hash": "bbf4578",
+    "raw": "961fc8f-dirty",
+    "hash": "961fc8f",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "bbf4578-dirty",
+    "suffix": "961fc8f-dirty",
     "semverString": null,
     "version": "1.0.0-alpha.0"
 };
