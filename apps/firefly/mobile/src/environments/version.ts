b5e472c2058a22ec4ac9085d8e242f6986a30c97
Andre Davcev <>
2018-11-20 18:57:58 -0500

    Move from folder apps/ionic-firefly to apps/firefly/mobile

---
+++ apps/firefly/mobile/src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "d4ba938-dirty",
+    "hash": "d4ba938",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "d4ba938-dirty",
+    "semverString": null,
+    "version": "1.0.0-alpha.0"
+};
+/* tslint:enable */
