2a64d71aa9344f2ce7fd287e3ff7a98fd9734ff1
Andre Davcev <>
2018-11-28 21:40:11 -0500

    Added action sheet to assets add button

--- apps/firefly/mobile/src/app/pages/user/user.page.ts
+++ apps/firefly/mobile/src/app/pages/user/user.page.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, ActionSheetController, NavController } from '@ionic/angular';
 import { StatusBarStyle } from '@capacitor/core';
 
 import { StatusBar } from '@theory/capacitor';
+import { from, Observable } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { PagesAssets } from '../publisher-assets';
 
 @Component
 ({
@@ -11,11 +14,35 @@ import { StatusBar } from '@theory/capacitor';
     styleUrls   : ['./user.page.scss']
 })
 
-export class PageUser
+export class PageUser implements OnInit
 {
     public segment: string = 'assets';
+    public translations: Array<string> = [];
 
-    constructor(private modalController: ModalController) { }
+    constructor
+    (
+        private modalController: ModalController,
+        private actionSheetController: ActionSheetController,
+        private nav: NavController,
+        private translate: TranslateService,
+    ) { }
+
+    ngOnInit(): void
+    {
+        this.translate.get
+        ([
+            'page.assets.add',
+            'page.assets.icon',
+            'page.assets.image',
+            'page.assets.coupon',
+            'page.assets.beacon',
+            'page.assets.place',
+            'page.assets.event',
+            'page.assets.cluster',
+            'general.cancel'
+        ]).
+        subscribe((translations: Array<string>) => this.translations = translations);
+    }
 
     ionViewWillEnter()
     {
@@ -31,5 +58,60 @@ export class PageUser
     {
         StatusBar.setStyle({style: StatusBarStyle.Light});
         this.modalController.dismiss();
+    }
+
+    public add(): void
+    {
+        const actionSheet$: Observable<HTMLIonActionSheetElement> = from(this.actionSheetController.create
+        ({
+            header : this.translations['page.assets.add'],
+
+            buttons :
+            [
+                {
+                    text    : this.translations['page.assets.cluster'],
+                    icon    : 'bug',
+                    handler : () => {this.nav.navigateForward([PagesAssets.Clusters]);}
+                },
+                {
+                    text    : this.translations['page.assets.event'],
+                    icon    : 'calendar',
+                    handler : () => {this.nav.navigateForward([PagesAssets.Events]);}
+                },
+                {
+                    text    : this.translations['page.assets.place'],
+                    icon    : 'pin',
+                    handler : () => {this.nav.navigateForward([PagesAssets.Places]);}
+                },
+                {
+                    text    : this.translations['page.assets.icon'],
+                    icon    : 'contact',
+                    handler : () => {this.nav.navigateForward([PagesAssets.Icons]);}
+                },
+                {
+                    text    : this.translations['page.assets.image'],
+                    icon    : 'images',
+                    handler : () => {this.nav.navigateForward([PagesAssets.Clusters]);}
+                },
+/*
+                {
+                    text    : this.translations['page.assets.coupon'],
+                    icon    : 'pricetags',
+                    handler : () => {this.nav.navigateForward([PagesAssets.Coupons]);}
+                },
+                {
+                    text    : this.translations['page.assets.beacon'],
+                    icon    : 'bluetooth',
+                    handler : () => {this.nav.navigateForward([PagesAssets.Beacons]);}
+                },
+*/
+                {
+                    text    : 'Cancel',
+                    role    : 'cancel'
+                }
+            ]
+        }));
+
+        actionSheet$.subscribe((actionSheet: HTMLIonActionSheetElement) => actionSheet.present());
     }
 }
