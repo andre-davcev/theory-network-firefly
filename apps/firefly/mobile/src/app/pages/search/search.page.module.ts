9160ef41f0025e3f51a537122ee312993cbcbe61
Andre Davcev <>
2018-12-04 21:19:20 -0500

    Fixed home screen navigation

--- apps/firefly/mobile/src/app/pages/search/search.page.module.ts
+++ apps/firefly/mobile/src/app/pages/search/search.page.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { ModulePage } from '@firefly/app/modules';
 
+import { RoutesPageSearch } from './search.page.routes';
 import { PageSearch } from './search.page';
 
 @NgModule
 ({
     imports :
     [
-        ModulePage
+        ModulePage,
+        RouterModule.forChild(RoutesPageSearch)
     ],
 
     declarations : [PageSearch],
