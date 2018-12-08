04d102dde1f8149ec21ff266c4109fb373064583
David Beaton <david.beaton88@gmail.com>
2018-12-08 02:38:35 -0500

    connected Stream button to launch blank page

--- apps/firefly/mobile/src/app/pages/home/home.page.routes.ts
+++ apps/firefly/mobile/src/app/pages/home/home.page.routes.ts
@@ -11,4 +11,5 @@ export const RoutesPageHome: Routes =
     { path: PagesHome.Publish,       loadChildren: '@firefly/page/cluster#ModulePagePublisherCluster' },
     { path: PagesHome.Assets,        loadChildren: '@firefly/page/user#ModulePageUser' },
     { path: PagesHome.User,          loadChildren: '@firefly/page/user#ModulePageUser' },
+    { path: PagesHome.Stream,        loadChildren: '@firefly/page/stream#ModulePageStream' },
 ];
