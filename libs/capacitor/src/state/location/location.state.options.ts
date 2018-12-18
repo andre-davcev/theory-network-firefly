3c61666baf28bd9ed69785446b5100709cdbcc63
Andre Davcev <>
2018-12-17 21:51:09 -0500

    Fixed location not updating bug

--- libs/capacitor/src/state/location/location.state.options.ts
+++ libs/capacitor/src/state/location/location.state.options.ts
@@ -8,16 +8,7 @@ export const StateLocationOptions: StoreOptions<StateLocationModel> =
 
     defaults :
     {
-        location  :
-        {
-            coords :
-            {
-                longitude: -77.594663683890587,
-                latitude: 43.127357144138806,
-                accuracy: 60
-            }
-        },
-
+        location : undefined,
         error : undefined
     }
 };
