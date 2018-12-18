51d1417b1e5137314ce3c986ed9a077f656ec0d6
Andre Davcev <>
2018-12-18 00:50:32 -0500

    Responsive home screen card height

--- libs/firefly/mobile/src/components/slide/slide.component.ts
+++ libs/firefly/mobile/src/components/slide/slide.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
 
 import { Slide } from './slide.interface';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component
 ({
@@ -10,11 +13,20 @@ import { Slide } from './slide.interface';
     changeDetection : ChangeDetectionStrategy.OnPush
 })
 
-export class ComponentSlide
+export class ComponentSlide implements OnInit
 {
     @Input() slide: Slide;
     @Input() buttonText: string;
     @Input() routerLink: string;
+    @Input() showMap: boolean = false;
 
-    constructor() { }
+    public breakpointShowMap$: Observable<boolean>;
+
+    constructor(private breakpointObserver: BreakpointObserver) { }
+
+    ngOnInit(): void
+    {
+        this.breakpointShowMap$ = this.breakpointObserver.observe(['(min-height: 890px)']).
+        pipe(map((state: BreakpointState) => state.matches));
+    }
 }
