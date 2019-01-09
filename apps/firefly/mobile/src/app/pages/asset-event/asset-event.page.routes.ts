bb669a3134d93e8fd46f7e5d4229623aa8f33d3b
Andre Davcev <>
2019-01-09 00:15:09 -0500

    FIR-1511 - Change select icon text to select cluster

--- apps/firefly/mobile/src/app/pages/asset-event/asset-event.page.routes.ts
+++ apps/firefly/mobile/src/app/pages/asset-event/asset-event.page.routes.ts
@@ -7,6 +7,6 @@ export const RoutesPageAssetEvent: Routes =
 [
     { path: '', component : PageAssetEvent },
 
-    { path: Pages.IconSelector,  loadChildren: '@firefly/page/icon-selector#ModulePageIconSelector' },
+    { path: Pages.Clusters,      loadChildren: '@firefly/page/assets-clusters#ModulePageAssetsClusters' },
     { path: Pages.ImageSelector, loadChildren: '@firefly/page/image-selector#ModulePageImageSelector' }
 ];
