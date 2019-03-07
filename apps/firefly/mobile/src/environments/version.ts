f65787475b5e61d569d12b5a4f9be6999a115f77
Andre Davcev <>
2019-03-06 20:07:13 -0500

    Upgraded to ionic-4.1.0 & capacitor-1.0.0-beta.18

--- apps/firefly/mobile/src/environments/version.ts
+++ apps/firefly/mobile/src/environments/version.ts
@@ -2,12 +2,12 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "697fde6-dirty",
-    "hash": "697fde6",
+    "raw": "85c6de3-dirty",
+    "hash": "85c6de3",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "697fde6-dirty",
+    "suffix": "85c6de3-dirty",
     "semverString": null,
     "version": "1.0.0-alpha.0"
 };
