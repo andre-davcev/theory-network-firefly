4bcdd76cc819d4c163d9cbb1c0d296cc341e47cd
Andre Davcev <>
2019-03-07 22:24:45 -0500

    Upgrade to ionic-4.1.1

--- apps/firefly/mobile/src/environments/version.ts
+++ apps/firefly/mobile/src/environments/version.ts
@@ -2,12 +2,12 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "85c6de3-dirty",
-    "hash": "85c6de3",
+    "raw": "3efa453-dirty",
+    "hash": "3efa453",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "85c6de3-dirty",
+    "suffix": "3efa453-dirty",
     "semverString": null,
     "version": "1.0.0-alpha.0"
 };
