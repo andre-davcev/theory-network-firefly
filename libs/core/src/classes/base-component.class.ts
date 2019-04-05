b3b14bb0778497a2902458c93f77299f4eb07b17
Andre Davcev <>
2019-04-04 20:39:02 -0400

    FIR-1636 - Update BaseComponent ngOnDestroy

--- libs/core/src/classes/base-component.class.ts
+++ libs/core/src/classes/base-component.class.ts
@@ -1,4 +1,4 @@
-import { OnDestroy } from '@angular/core';
+import { OnDestroy, HostListener } from '@angular/core';
 
 // libs
 import { Subject } from 'rxjs';
@@ -6,6 +6,7 @@ import { Subject } from 'rxjs';
 export abstract class BaseComponent implements OnDestroy {
   public destroy$: Subject<any> = new Subject();
 
+  @HostListener('window:beforeunload')
   ngOnDestroy() {
     this.destroy$.next(true);
     this.destroy$.complete();
