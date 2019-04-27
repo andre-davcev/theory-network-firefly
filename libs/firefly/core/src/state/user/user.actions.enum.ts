9cabd74edf9518cccf455c024c3a309854ecbeb7
Andre Davcev <>
2019-04-27 16:16:53 -0400

    User state refactor

--- libs/firefly/core/src/state/user/user.actions.enum.ts
+++ libs/firefly/core/src/state/user/user.actions.enum.ts
@@ -2,7 +2,8 @@ export enum ActionsUser
 {
     UserAuthenticate      = '[User] User Authenticate',
     UserAuthenticateCheck = '[User] User Authenticate Check',
-    UserGet               = '[User] User Get',
+    UserWatch             = '[User] User Watch',
+    UserWatchLanguage     = '[User] User Watch Language',
     UserCreate            = '[User] User Create',
     UserAddToken          = '[User] User Add Token',
     LoginEmail            = '[User] Login Email',
--- libs/firefly/core/src/state/user/user.state.model.ts
+++ libs/firefly/core/src/state/user/user.state.model.ts
@@ -1,4 +1,6 @@
-import { User } from '@firefly/core/models';
+import { User, UserKey } from '@firefly/core/models';
+import { ModelKey } from '@theory/firebase';
+import { FormGroup } from '@angular/forms';
 
 export interface StateUserModel
 {
@@ -8,4 +10,27 @@ export interface StateUserModel
     authenticated  : boolean;
     authenticating : boolean;
     initializing   : boolean;
+    form           : FormGroup,
+
+    empty    :
+    {
+        [ModelKey.Version]     : '1.0.0',
+        [ModelKey.Id]          : undefined,
+        [ModelKey.DateCreated] : undefined,
+        [ModelKey.DateUpdated] : undefined,
+
+        [UserKey.Uid]           : undefined,
+        [UserKey.Language]      : 'en',
+        [UserKey.DisplayName]   : undefined,
+        [UserKey.Email]         : undefined,
+        [UserKey.PhoneNumber]   : undefined,
+        [UserKey.PhotoUrl]      : undefined,
+        [UserKey.ProviderId]    : undefined,
+        [UserKey.Tokens]        : {},
+        [UserKey.Notifications] : {},
+        [UserKey.Clusters]      : {},
+        [UserKey.Events]        : {},
+        [UserKey.Images]        : {},
+        [UserKey.Icons]         : {}
+    }
 }
