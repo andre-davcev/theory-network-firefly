58480d240391e588c46fb68013d62de2bbee864b
Andre Davcev <>
2019-04-28 16:54:55 -0400

    More infra

--- libs/firefly/core/src/state/event/event.actions.enum.ts
+++ libs/firefly/core/src/state/event/event.actions.enum.ts
@@ -1,13 +1,14 @@
 export enum ActionsEvent
 {
-    GetEvents        = '[Event] Get Event',
-    SetId            = '[Event] Set Id',
-    SetEvent         = '[Event] Set Event',
-    WatchEvent       = '[Event] Watch Event',
-    SaveEvent        = '[Event] Save Event',
-    CreateEvent      = '[Event] Create Event',
-    UpdateEvent      = '[Event] Update Event',
-    SetEventLocation = '[Event] Set Event Location',
-    PatchEvent       = '[Event] Patch Event',
-    SetImage         = '[Event] Set Image Index'
+    GetEvents         = '[Event] Get Event',
+    SetId             = '[Event] Set Id',
+    SetEvent          = '[Event] Set Event',
+    WatchEvent        = '[Event] Watch Event',
+    SaveEvent         = '[Event] Save Event',
+    CreateEvent       = '[Event] Create Event',
+    UpdateEvent       = '[Event] Update Event',
+    SetEventLocation  = '[Event] Set Event Location',
+    PatchEvent        = '[Event] Patch Event',
+    SetImage          = '[Event] Set Image Index',
+    SetClusterPrimary = '[Event] Set Cluster Primary'
 }
--- libs/firefly/core/src/state/event/event.state.model.ts
+++ libs/firefly/core/src/state/event/event.state.model.ts
@@ -1,6 +1,6 @@
 import { FormGroup } from '@angular/forms';
 
-import { Event } from '@firefly/core/models';
+import { Event, Cluster } from '@firefly/core/models';
 
 export interface StateEventModel
 {
@@ -8,4 +8,5 @@ export interface StateEventModel
     empty              : Event;
     imageUrl           : string;
     imageUrlNormalized : string;
+    clusterPrimary     : Cluster;
 }
