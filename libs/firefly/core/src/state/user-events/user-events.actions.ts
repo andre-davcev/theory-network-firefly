8525ddac9367ac445a6fc731db2d18086b84fb51
Andre Davcev <>
2019-08-28 18:50:31 -0400

    Cleanup of StateUserEvents

--- libs/firefly/core/src/state/user-events/user-events.actions.ts
+++ libs/firefly/core/src/state/user-events/user-events.actions.ts
@@ -4,8 +4,8 @@ import { SortField } from '@theory/state';
 
 export class ActionUserEventsReset     { static readonly type = ActionsUserEvents.Reset;     constructor() { } }
 export class ActionUserEventsGetData   { static readonly type = ActionsUserEvents.GetData;   constructor() { } }
+export class ActionUserEventsGet       { static readonly type = ActionsUserEvents.Get;       constructor() { } }
 export class ActionUserEventsSet       { static readonly type = ActionsUserEvents.Set;       constructor(public payload: Record<string, UserEvent>) { } }
 export class ActionUserEventsSort      { static readonly type = ActionsUserEvents.Sort;      constructor(public payload?: SortField) { } }
-export class ActionUserEventsGet       { static readonly type = ActionsUserEvents.Get;       constructor() { } }
 export class ActionUserEventsAdd       { static readonly type = ActionsUserEvents.Add;       constructor(public payload: Event) { } }
 export class ActionUserEventsRemove    { static readonly type = ActionsUserEvents.Remove;    constructor(public payload: string) { } }
