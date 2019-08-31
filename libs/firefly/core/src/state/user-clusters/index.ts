a3b23e48b6148698851b2b275d42aaa3c04aa207
Andre Davcev <>
2019-08-31 00:41:28 -0400

    StateUserClusters

---
+++ libs/firefly/core/src/state/user-clusters/index.ts
@@ -0,0 +1,5 @@
+export * from './user-clusters.actions.enum';
+export * from './user-clusters.actions';
+export * from './user-clusters.state';
+export * from './user-clusters.state.model';
+export * from './user-clusters.state.options';
---
+++ libs/firefly/core/src/state/user-clusters/user-clusters.actions.enum.ts
@@ -0,0 +1,10 @@
+export enum ActionsUserClusters
+{
+    Reset     = '[UserClusters] Reset',
+    GetData   = '[UserClusters] Get Data',
+    Get       = '[UserClusters] Get',
+    Set       = '[UserClusters] Set',
+    Sort      = '[UserClusters] Sort',
+    Add       = '[UserClusters] Add',
+    Remove    = '[UserClusters] Remove'
+}
