4a2bbb71e26cf930d92f7bce7a02d34382c67897
davidbeaton88 <david.beaton88@gmail.com>
2019-10-13 18:55:30 -0400

    updated login screen to create and login with user

--- libs/firefly/core/src/state/user/user.actions.ts
+++ libs/firefly/core/src/state/user/user.actions.ts
@@ -34,7 +34,7 @@ export class ActionUserCreate
 {
     static readonly type = ActionsUser.UserCreate;
 
-    constructor() {}
+    constructor(public payload: Credentials) {}
 }
 
 export class ActionUserAddToken
