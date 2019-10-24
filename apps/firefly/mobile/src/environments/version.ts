f577e1adb6c6ce86aca3cb2538ad86ca89489e5b
Andre Davcev <>
2019-10-24 02:11:29 -0400

    Intermediate commit

--- apps/firefly/mobile/src/environments/version.ts
+++ apps/firefly/mobile/src/environments/version.ts
@@ -2,12 +2,12 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "8de7c95-dirty",
-    "hash": "8de7c95",
+    "raw": "85e3257-dirty",
+    "hash": "85e3257",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "8de7c95-dirty",
+    "suffix": "85e3257-dirty",
     "semverString": null,
     "version": "1.0.0-alpha.0"
 };
