fa000f841b24fb0613e2431b905083b5568db78d
Andre Davcev <>
2019-10-28 22:38:25 -0400

    Add deep value support for firestore object sorting

--- libs/core/src/utils/core.util.ts
+++ libs/core/src/utils/core.util.ts
@@ -4,6 +4,20 @@ export class CoreUtil
     {
         return JSON.parse(JSON.stringify(object));
     }
+
+    public static deepValue(key: string, object: Record<string, any>): any
+    {
+        let value = object;
+
+        key.
+        split('.').
+        forEach((k: string) =>
+            value == null ? undefined : value[k]
+        );
+
+        return value;
+    }
+
     public static page<T>(sourceList: Array<T>, pagedList: Array<any>, pageSize: number, page: number): Array<T>
     {
         const difference: number = sourceList.length - pagedList.length;
