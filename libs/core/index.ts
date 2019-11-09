e111e4b18ffe1fca8456d3ecf1553723ed5458bd
Andre Davcev <>
2019-11-09 12:55:09 -0500

    Random cleanup

--- libs/core/index.ts
+++ libs/core/index.ts
@@ -1,9 +1,6 @@
 export * from './src/classes';
 export * from './src/enums';
-export * from './src/environments';
 export * from './src/features';
 export * from './src/interfaces';
-export * from './src/services';
 export * from './src/utils';
 export * from './src/validators';
-export { CoreModule } from './src/core.module';
