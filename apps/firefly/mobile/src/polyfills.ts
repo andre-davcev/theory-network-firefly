596b73d52d74fe4ff3ea9e505d8dacf242afe6d1
Andre Davcev <>
2019-11-08 19:31:13 -0500

    Removed references to @ionic-native & cordova

--- apps/firefly/mobile/src/polyfills.ts
+++ apps/firefly/mobile/src/polyfills.ts
@@ -72,7 +72,6 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-//import '@ionic/pwa-elements';
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
 
 // https://github.com/Wykks/ngx-mapbox-gl/issues/136
