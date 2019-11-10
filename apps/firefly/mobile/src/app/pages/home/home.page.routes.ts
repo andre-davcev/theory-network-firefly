b68ccd93ebef3587ece1dfa6b07a428075150a76
Andre Davcev <>
2019-11-09 22:03:15 -0500

    Updated to new import syntax

--- apps/firefly/mobile/src/app/pages/home/home.page.routes.ts
+++ apps/firefly/mobile/src/app/pages/home/home.page.routes.ts
@@ -8,7 +8,7 @@ export const RoutesPageHome: Routes =
     { path : '', component : PageHome,
       children: [
         { path: '',           redirectTo: Pages.Stream, pathMatch: 'full' },
-        { path: Pages.Stream, loadChildren: '@firefly/page/stream#ModulePageStream' },
-        { path: Pages.Alert,  loadChildren: '@firefly/page/alert#ModulePageAlert' }
+        { path: Pages.Stream, loadChildren: () => import(`@firefly/page/stream`).then(m => m.ModulePageStream) },
+        { path: Pages.Alert,  loadChildren: () => import(`@firefly/page/alert`).then(m => m.ModulePageAlert) }
     ]}
 ];
