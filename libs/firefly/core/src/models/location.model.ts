b82340cfd76ef0ada57a1ccb87c724d8ea8a4cf1
Andre Davcev <>
2019-12-09 17:43:11 -0500

    Update changes folder

--- libs/firefly/core/src/models/location.model.ts
+++ libs/firefly/core/src/models/location.model.ts
@@ -3,6 +3,7 @@ import { firestore } from 'firebase/app';
 export interface Location
 {
     geopoint:  firestore.GeoPoint
+    cityId:    string;
     city:      string;
     region:    string;
     country:   string;
