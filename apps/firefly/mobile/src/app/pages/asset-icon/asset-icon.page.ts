be0a7f702d8c496a73b0ca078109d8a8f304e86c
davidbeaton88 <david.beaton88@gmail.com>
2019-12-11 18:40:16 -0500

    set icon url when adding a new icon

--- apps/firefly/mobile/src/app/pages/asset-icon/asset-icon.page.ts
+++ apps/firefly/mobile/src/app/pages/asset-icon/asset-icon.page.ts
@@ -6,7 +6,7 @@ import { FormGroup } from '@angular/forms';
 import { ModalController } from '@ionic/angular'
 import { StatusBarStyle, CameraPhoto, CameraSource, CameraResultType, CameraOptions, Plugins } from '@capacitor/core';
 import { ActionDeviceStatusBarSet, StateDevice } from '@theory/capacitor';
-import { StateIcon, ActionUserIconsReset, ActionClusterIconUriSet, ActionIconSetId, MockIconId } from '@firefly/core';
+import { StateIcon, ActionUserIconsReset, ActionIconUriSet, ActionClusterIconPathSet, ActionIconSetId, MockIconId } from '@firefly/core';
 import { ActionMobileLoadingShow, ActionMobileLoadingHide } from '@firefly/mobile';
 import { Pages } from '../pages.enum';
 
@@ -21,7 +21,10 @@ const { Camera } = Plugins;
 
 export class PageAssetIcon
 {
-    @Select(StateIcon.formGroup) form$: Observable<FormGroup>;
+    @Select(StateIcon.formGroup()) form$: Observable<FormGroup>;
+    @Select(StateIcon.iconUrl) iconUrl$: Observable<string>;
+
+    public Pages: any = Pages;
 
     constructor(private store: Store, private modal: ModalController)
     {
@@ -53,7 +56,7 @@ export class PageAssetIcon
                   switchMap(() => from(Camera.getPhoto(options))),
                   map((photo: CameraPhoto) => photo.dataUrl),
                   switchMap((imageData: string) =>
-                      this.store.dispatch(new ActionClusterIconUriSet(imageData))
+                      this.store.dispatch(new ActionIconUriSet(imageData))
                   ),
                   finalize(() => {
                     this.modal.dismiss();
@@ -64,8 +67,14 @@ export class PageAssetIcon
           }
           else
           {
-              this.store.dispatch(new ActionIconSetId(MockIconId));
+              this.modal.dismiss();
+              this.store.dispatch(new ActionClusterIconPathSet()).
+              subscribe();
           }
-        }
+      }
+      else
+      {
+          this.store.dispatch(new ActionIconSetId(MockIconId));
+      }
     }
 }
