f436928710cec7db840fbed37b3b397719bcca3c
Andre Davcev <>
2019-12-11 18:56:48 -0500

    Intermediate commit

--- apps/firefly/cloud/src/library/services/cities.services.ts
+++ apps/firefly/cloud/src/library/services/cities.services.ts
@@ -1,31 +1,36 @@
 import { WriteResult, Firestore, DocumentSnapshot } from '@google-cloud/firestore';
-import { firestore as db } from 'firebase-admin';
 
 export class ServiceCities
 {
-    public static async createIfNew(database: Firestore, city: Record<string, any>): Promise<WriteResult>
+    public static async createIfNew(database: Firestore, event: Record<string, any>): Promise<WriteResult>
     {
-        const cityDoc: DocumentSnapshot = await database.collection('cities').doc(city.cityId).get();
+        const city:         Record<string, any> = event.city;
+        const clustersList: Array<string>       = event.clusters;
+        const cityDoc:      DocumentSnapshot    = await database.collection('cities').doc(city.cityId).get();
 
-        return cityDoc.exists ?
-            null :
-            cityDoc.ref.create
-            ({
-                geopoint:  city.geopoint,
-                city:      city.city,
-                region:    city.region,
-                country:   city.country,
-                nearby:    [],
+        if (cityDoc.exists) { return null; }
 
-                clusters :
-                {
-                    [city.clusters[0]]:
-                    {
-                        events: 1,
-                        total:  1,
-                        ratio:  1
-                    }
-                }
-            });
+        const clusters: Record<string, any> = {};
+
+        clustersList.forEach((clusterId: string) =>
+            clusters[clusterId] =
+            {
+                events: 1,
+                total:  1,
+                ratio:  1
+            }
+        );
+
+        return cityDoc.ref.create
+        ({
+            geopoint:  city.geopoint,
+            city:      city.city,
+            region:    city.region,
+            country:   city.country,
+            nearby:    [],
+
+            clusters,
+            clustersList
+        });
     }
 }
