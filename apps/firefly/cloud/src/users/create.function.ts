72109e03fb092625da6a741e15974a4f5ef0136f
Andre Davcev <>
2019-12-19 22:02:56 -0500

    Update user create cloud function to create first alert

--- apps/firefly/cloud/src/users/create.function.ts
+++ apps/firefly/cloud/src/users/create.function.ts
@@ -1,7 +1,8 @@
 import { firestore, EventContext, CloudFunction } from 'firebase-functions';
-import { DocumentSnapshot, Firestore, DocumentReference } from '@google-cloud/firestore';
+import { DocumentSnapshot, Firestore, DocumentReference, WriteResult, FieldValue } from '@google-cloud/firestore';
 import { firestore as db } from 'firebase-admin';
-import { ServiceFirestore, Version } from '../library';
+import { ServiceFirestore, Version, Cluster, Alert } from '../library';
+import { User } from 'firebase';
 
 const database: Firestore = db();
 
@@ -11,45 +12,46 @@ firestore.
 document('users/{id}').
 onCreate(async (snapshot: DocumentSnapshot, context: EventContext) =>
 {
-    const id: string = snapshot.id;
+    const id   : string = snapshot.id;
+    const user : User   = ServiceFirestore.create<User>(snapshot, { version: Version.Users } as Partial<User>);
 
-    const user: any = ServiceFirestore.create(snapshot,
+    const documentCluster : DocumentReference = database.collection('clusters').doc();
+    const documentAlert   : DocumentReference = database.collection('alerts').doc();
+
+    const cluster: Partial<Cluster> =
     {
-        version: Version.Users
-    });
+        userId : id,
 
-    await Promise.all
+        name            : 'Your first cluster!',
+        tagline         : 'Come enjoy my first event cluster',
+        description     : `This is your first cluster. When you're ready to publish to the global catalog, flip off the private switch and join the Firefly community of publishers!`,
+        bucketPath      : 'baLysAd71cRyZjh0hr6poxR8an13_icons_CASwQmcg46JQYZqGmC3e.png',
+        private         : true,
+        subscriberCount : 0
+    };
+
+    const alert: Partial<Alert> =
+    {
+        userId : id,
+
+        name        : 'Your first alert!',
+        description : `This is your first alert. Once you subscribe to Firefly Clusters found on the screen in Discover, you will receive push notifications`,
+        bucketPath  : 'baLysAd71cRyZjh0hr6poxR8an13/images/nA8wxI4UIhEU0YjfwWPe.jpeg',
+        eventId     : null, // ToDo: Add default eventId here
+        clusterId   : documentCluster.id,
+        dateTime    : FieldValue.serverTimestamp(),
+        read        : false,
+        tokens      : [],
+        url         : 'https://firefly.im'
+    };
+
+    return Promise.all
     ([
         snapshot.ref.update(user),
-
-        database.collection('user-alerts').doc(id).create({}),
-        database.collection('user-clusters').doc(id).create({}),
-        database.collection('user-events').doc(id).create({}),
-        database.collection('user-icons').doc(id).create({}),
-        database.collection('user-images').doc(id).create({}),
         database.collection('user-profiles').doc(id).create({}),
-        database.collection('user-roles').doc(id).create({ admins: {}, editors: {} }),
-        database.collection('user-stream').doc(id).create({}),
-        database.collection('user-subscriptions').doc(id).create({}),
+        documentCluster.set(cluster),
+        documentAlert.set(alert)
     ]);
-
-    const timestamp: db.FieldValue     = db.FieldValue.serverTimestamp();
-    const document:  DocumentReference = database.collection('clusters').doc();
-
-    return database.collection('clusters').add
-    ({
-        id          : document.id,
-        version     : Version.Clusters,
-        name        : 'Your first cluster!',
-        description : `This is your first cluster. When you're ready to publish to the global catalog, flip off the private switch and join the Firefly community of publishers!`,
-        private     : true,
-        userId      : id,
-        draft       : false,
-        tagline     : 'Come enjoy my first event cluster',
-        iconId      : 'baLysAd71cRyZjh0hr6poxR8an13_icons_CASwQmcg46JQYZqGmC3e.png',
-        dateCreated : timestamp,
-        dateUpdated : timestamp
-    });
 });
 
 export { UsersCreate };
