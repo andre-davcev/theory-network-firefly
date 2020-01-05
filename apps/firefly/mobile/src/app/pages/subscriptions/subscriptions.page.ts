9a06203a5ca781644382a2478fd0fb207b7d20ae
davidbeaton88 <david.beaton88@gmail.com>
2020-01-05 17:48:51 -0500

    go to cluster detail when selecting subscription from manage subscriptions

--- apps/firefly/mobile/src/app/pages/subscriptions/subscriptions.page.ts
+++ apps/firefly/mobile/src/app/pages/subscriptions/subscriptions.page.ts
@@ -3,14 +3,15 @@ import { StatusBarStyle } from '@capacitor/core';
 
 import { ActionDeviceStatusBarSet } from '@theory/capacitor';
 import { Store, Select } from '@ngxs/store';
-import { Observable } from 'rxjs';
-import { StateMobile } from '@firefly/mobile';
+import { Observable, of } from 'rxjs';
+import { StateMobile, Pages, ActionMobileLoadingShow, ActionMobileLoadingHide } from '@firefly/mobile';
 import { MenuController } from '@ionic/angular';
-import { StateUserSubscriptions, ActionUserSubscriptionToggle, ActionUserSubscriptionsReset, ActionUserWatchSubscriptionsStatus } from '@firefly/core';
+import { StateUserSubscriptions, ActionUserSubscriptionToggle, ActionUserSubscriptionsReset, ActionUserWatchSubscriptionsStatus, ActionClusterSetId, StateUserClusters, ActionUserClustersGetData } from '@firefly/core';
 import { Subscription } from '@firefly/cloud';
 import { BaseComponent } from '@theory/core';
 import { StateStorage, StorageImage } from '@theory/firebase';
-import { takeUntil, switchMap } from 'rxjs/operators';
+import { takeUntil, switchMap, take } from 'rxjs/operators';
+import { Navigate } from '@ngxs/router-plugin';
 
 @Component
 ({
@@ -24,6 +25,7 @@ export class PageSubscriptions extends BaseComponent implements OnInit
     @Select(StateUserSubscriptions.data()) subscriptions$: Observable<Array<Subscription>>;
     @Select(StateStorage.images)           images$:   Observable<Record<string, StorageImage>>;
     @Select(StateMobile.menuOpen)          menuOpen$: Observable<boolean>;
+    @Select(StateUserClusters.initialized()) clustersInitialized$: Observable<boolean>;
 
     public images: Record<string, StorageImage> = {};
 
@@ -58,5 +60,27 @@ export class PageSubscriptions extends BaseComponent implements OnInit
     public toggle(event, subscription: Subscription)
     {
       this.store.dispatch(new ActionUserSubscriptionToggle(subscription.id));
+    }
+
+    public select(subscription: Subscription): void
+    {
+      this.clustersInitialized$.
+        pipe
+        (
+            take(1),
+            switchMap((initialized: boolean) =>
+                initialized ?
+                    of(null) :
+                    this.store.dispatch(new ActionMobileLoadingShow()).
+                    pipe
+                    (
+                        switchMap(() => this.store.dispatch(new ActionUserClustersGetData())),
+                        switchMap(() => this.store.dispatch(new ActionMobileLoadingHide()))
+                    )
+            ),
+            switchMap(() =>
+            this.store.dispatch(new Navigate([Pages.AssetCluster], {id: subscription.id}, {state: {isClusterDetail:true}}))
+            )
+        ).subscribe();
     }
 }
