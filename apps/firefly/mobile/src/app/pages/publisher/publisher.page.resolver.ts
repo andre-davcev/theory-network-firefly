009d097a7b179e07da564d1d1eca0c69b75cde55
Andre Davcev <>
2020-01-18 18:10:00 -0500

    Minor tweaks

---
+++ apps/firefly/mobile/src/app/pages/publisher/publisher.page.resolver.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ResolverPagePublisher implements Resolve<boolean>
+{
+    constructor() {}
+
+    public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>
+    {
+        return of(true);
+    }
+}
