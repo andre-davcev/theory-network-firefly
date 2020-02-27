8a6b601845f7eba2c13c58a40444bc1c928cb79d
davidbeaton88 <david.beaton88@gmail.com>
2020-02-26 23:41:05 -0500

    mark alert as read when swiping or loading first slide and update unread count. When deleting, update unread count.

--- apps/firefly/mobile/src/app/pages/alert/alert.page.routes.ts
+++ apps/firefly/mobile/src/app/pages/alert/alert.page.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { PageAlert } from './alert.page';
+import { ResolverPageAlert } from './alert.page.resolver';
 
 export const RoutesPageAlert: Routes =
 [
-    { path : '', component : PageAlert}
+    { path : '', component : PageAlert, resolve: { loader: ResolverPageAlert }}
 ];
--- apps/firefly/mobile/src/app/pages/alert/alert.page.ts
+++ apps/firefly/mobile/src/app/pages/alert/alert.page.ts
@@ -1,15 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
-import { tap, switchMap } from 'rxjs/operators';
+import { Component, ViewChild, ViewChildren, OnInit } from '@angular/core';
+import { tap, switchMap, takeUntil, filter } from 'rxjs/operators';
 import { IonSlides, ModalController } from '@ionic/angular';
 import { Select, Store } from '@ngxs/store';
-import { Observable, from } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
 
-import { StateUserAlerts, ActionEventGet, ActionUserAlertsGo, IconType, ActionUserAlertsRemove, ActionAlertSetId, ActionAlertDelete, ActionUserAlertsMarkRead } from '@firefly/core';
+import { StateUserAlerts, ActionEventGet, ActionUserAlertsGo, IconType, ActionUserAlertsRemove, ActionAlertSetId, ActionAlertDelete, ActionUserAlertsMarkRead, ActionAlertMarkRead, StateAlert } from '@firefly/core';
 import { Alert } from '@firefly/cloud';
 
 import { Pages } from '@firefly/mobile';
 import { Navigate } from '@ngxs/router-plugin';
 import { PageAlertDetail } from '../alert-detail/alert-detail.page';
+import { FormGroup } from '@angular/forms';
+import { BaseComponent } from '@theory/core';
 @Component
 ({
     selector    : 'app-page-alert',
@@ -17,33 +19,85 @@ import { PageAlertDetail } from '../alert-detail/alert-detail.page';
     styleUrls   : ['./alert.page.scss']
 })
 
-export class PageAlert
+export class PageAlert extends BaseComponent implements OnInit
 {
-    @Select(StateUserAlerts.data()) alerts$: Observable<Array<Alert>>;
-    @Select(StateUserAlerts.found()) found$: Observable<boolean>;
-    @Select(StateUserAlerts.empty()) empty$: Observable<boolean>;
+    @Select(StateAlert.formGroup())  form$:   Observable<FormGroup>;
+    @Select(StateUserAlerts.data())  alerts$: Observable<Array<Alert>>;
+    @Select(StateUserAlerts.found()) found$:  Observable<boolean>;
+    @Select(StateUserAlerts.empty()) empty$:  Observable<boolean>;
 
-    @ViewChild(IonSlides, { static: true }) slides: IonSlides;
+    @ViewChild('sliderRef', { static: true }) protected sliderRef: IonSlides;
 
     public segment: string = 'fired';
     public Pages: any = Pages;
     public slideOptions: any = { zoom: false };
+    public alerts: Array<Alert>;
 
     public IconType : any = IconType;
 
-    constructor(private store: Store, private modal: ModalController) { }
-
-    public slideChanged(): void
+    constructor(private store: Store, private modal: ModalController)
     {
-        from(this.slides.getActiveIndex()).
+      super();
+    }
 
-        pipe(tap((index: number) => console.log('Slide Changed: ' + index)));
+    public ngOnInit(): void
+    {
+      this.alerts$
+      .pipe(takeUntil(this.destroy$)).
+        subscribe((alerts: Array<Alert>) =>
+          this.alerts = alerts
+      )
+    }
+
+    public async ionViewWillEnter(): Promise<void>
+    {
+      const index = await this.sliderRef.getActiveIndex();
+
+      if(this.alerts[index].read)
+        return;
+
+        this.store.dispatch(new ActionAlertSetId(this.alerts[index].id)).pipe
+        (
+          switchMap(() =>
+            this.store.dispatch(new ActionAlertMarkRead())
+          ),
+          switchMap(() =>
+            this.store.dispatch(new ActionUserAlertsMarkRead())
+          )
+        ).subscribe();
+    }
+
+    public async slideChanged(): Promise<void>
+    {
+      const index = await this.sliderRef.getActiveIndex();
+
+      if(this.alerts[index].read)
+        return;
+
+      this.store.dispatch(new ActionAlertSetId(this.alerts[index].id)).pipe
+      (
+        switchMap(() =>
+          this.store.dispatch(new ActionAlertMarkRead())
+        ),
+        switchMap(() =>
+          this.store.dispatch(new ActionUserAlertsMarkRead())
+        )
+      ).subscribe();
     }
 
     public alertDetail(alert:Alert): void
     {
       this.store.dispatch(new ActionEventGet(alert.eventId)).pipe
       (
+        switchMap(() =>
+          this.store.dispatch(new ActionAlertSetId(alert.id))
+        ),
+        switchMap(() =>
+          this.store.dispatch(new ActionAlertMarkRead())
+        ),
+        switchMap(() =>
+        this.store.dispatch(new ActionUserAlertsMarkRead())
+        ),
         switchMap(() =>
           from(this.modal.create({
             component: PageAlertDetail
@@ -60,11 +114,16 @@ export class PageAlert
     public alertDelete(alert:Alert): void
     {
       const id: string = alert.id;
+
       this.store.dispatch(new ActionAlertSetId(id)).pipe
       (
         switchMap(() =>
           this.store.dispatch(new ActionAlertDelete())
         ),
+        switchMap(() =>
+          of(alert)
+        ),
+        filter((alert) => !alert.read),
         switchMap(() =>
           this.store.dispatch(new ActionUserAlertsMarkRead())
         )
