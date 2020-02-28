3bcf8cf71b7fed47330e27765b761e1ea08a673c
Andre Davcev <>
2020-02-27 21:41:29 -0500

    Update to ionic 5

--- apps/firefly/mobile/src/app/pages/alert/alert.page.routes.ts
+++ apps/firefly/mobile/src/app/pages/alert/alert.page.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { PageAlert } from './alert.page';
-import { ResolverPageAlert } from './alert.page.resolver';
+// import { ResolverPageAlert } from './alert.page.resolver';
 
 export const RoutesPageAlert: Routes =
 [
-    { path : '', component : PageAlert, resolve: { loader: ResolverPageAlert }}
+    { path : '', component : PageAlert, /*resolve: { loader: ResolverPageAlert } */ }
 ];
