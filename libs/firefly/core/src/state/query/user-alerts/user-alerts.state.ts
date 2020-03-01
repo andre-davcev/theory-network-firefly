b5141f8dcc3b8f1b1d6ac3d4a8783bdb41ca5394
davidbeaton88 <david.beaton88@gmail.com>
2020-03-01 16:42:00 -0500

    added some styling to read alerts list

--- libs/firefly/core/src/state/query/user-alerts/user-alerts.state.ts
+++ libs/firefly/core/src/state/query/user-alerts/user-alerts.state.ts
@@ -89,10 +89,13 @@ export class StateUserAlerts extends StateQuery<Alert, StateUserAlertsModel>
                 const language: string = this.store.selectSnapshot(StateLanguage.language);
                 const options: any = { weekday: 'long',
                 year: 'numeric', month: 'long', day: 'numeric'};
+                const optionsShort: any = { weekday: 'short',
+                year: 'numeric', month: 'short', day: 'numeric'};
 
                 let timeStart: Date;
                 let timeStartPrevious: Date;
                 let timeStartFormatted: string;
+                let timeStartFormattedShort: string;
 
                 let unread : number = StateUserAlerts.unread(state);
 
@@ -102,6 +105,7 @@ export class StateUserAlerts extends StateQuery<Alert, StateUserAlertsModel>
 
                     timeStart = (alert.dateTime as firestore.Timestamp).toDate();
                     timeStartFormatted = timeStart.toLocaleDateString(language, options);
+                    timeStartFormattedShort = timeStart.toLocaleDateString(language, optionsShort);
 
                     alert.metadata =
                     {
@@ -112,6 +116,7 @@ export class StateUserAlerts extends StateQuery<Alert, StateUserAlertsModel>
                     if(timeStartPrevious === undefined || timeStart.getTime() != timeStartPrevious.getTime())
                     alert.metadata.timeStartFormatted = timeStartFormatted;
 
+                    alert.metadata.timeStartFormattedShort = timeStartFormattedShort;
                     alert.metadata.timeStartDate = timeStart;
                     timeStartPrevious = timeStart;
                 });
