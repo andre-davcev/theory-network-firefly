5842a2445e73bacf34322debf9973b6b40c928de
Andre Davcev <>
2020-03-04 22:53:50 -0500

    Prepare route url for adding slide index

--- libs/firefly/mobile/src/state/mobile/mobile.actions.ts
+++ libs/firefly/mobile/src/state/mobile/mobile.actions.ts
@@ -8,6 +8,6 @@ export class ActionMobileLoadingHide  { static readonly type = ActionsMobile.Loa
 export class ActionMobileToast        { static readonly type = ActionsMobile.Toast;        constructor(public payload: string) {} }
 export class ActionMobileMenuOpened   { static readonly type = ActionsMobile.MenuOpened;   constructor() {} }
 export class ActionMobileMenuClosed   { static readonly type = ActionsMobile.MenuClosed;   constructor() {} }
-export class ActionMobileNavigateRoot { static readonly type = ActionsMobile.NavigateRoot; constructor(public page: Pages, public child?: Pages ) {} }
+export class ActionMobileNavigateRoot { static readonly type = ActionsMobile.NavigateRoot; constructor(public page: Pages, public child?: string ) {} }
 export class ActionMobileAuthSelect   { static readonly type = ActionsMobile.AuthSelect;   constructor() {} }
 export class ActionMobileAuthSelected { static readonly type = ActionsMobile.AuthSelected; constructor(public page: Pages.Login | Pages.SignUp) {} }
