01a1c5efd89517d16778b306a0a074134b1dc321
Andre Davcev <>
2020-03-07 11:57:05 -0500

    iOS push notifications prep

--- libs/firefly/mobile/src/state/notifications/notifications.state.ts
+++ libs/firefly/mobile/src/state/notifications/notifications.state.ts
@@ -1,20 +1,19 @@
 
 import { State, Selector, Action, StateContext, Store } from '@ngxs/store';
-import { Platform } from '@ionic/angular';
 
 import { PushNotification } from '@theory/firebase';
 
 import { StateNotificationsModel } from './notifications.state.model';
 import { StateNotificationsOptions } from './notifications.state.options';
 import { ActionNotificationsWatch } from './notifications.actions';
-import { Plugins, PushNotification as CapPushNotification } from '@capacitor/core';
+import { Plugins } from '@capacitor/core';
 import { FCM } from 'capacitor-fcm';
 import { ActionUserAddToken } from '@firefly/core';
 import { StateUser } from '@firefly/core';
 import { Injectable } from '@angular/core';
 import { StateDevice } from '@theory/capacitor';
-import { of, from, combineLatest } from 'rxjs';
-import { filter, catchError, switchMap, take, mergeMap, map } from 'rxjs/operators';
+import { of, from, forkJoin } from 'rxjs';
+import { filter, switchMap, take, map } from 'rxjs/operators';
 
 const fcm = new FCM();
 const { PushNotifications } = Plugins;
@@ -45,14 +44,14 @@ export class StateNotifications
                 from(PushNotifications.register())
             ),
             switchMap(() =>
-                combineLatest
+                forkJoin
                 ([
                     from(fcm.getToken()).pipe(map((response: { token: string }) => response.token)),
                     this.store.selectOnce(StateUser.tokens)
                 ])
             ),
             switchMap(([token, tokens]) =>
-                tokens && tokens.includes(token) ?
+                token && tokens && tokens.includes(token) ?
                     of(null) :
                     dispatch(new ActionUserAddToken(token))
             )
