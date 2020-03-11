dbf98627878e72d8930006bd5f8ddaac3909483a
davidbeaton88 <david.beaton88@gmail.com>
2020-03-10 20:09:41 -0400

    fixed strange transition from stream to subscriptions

--- apps/firefly/mobile/src/app/pages/subscriptions/subscriptions.page.resolver.ts
+++ apps/firefly/mobile/src/app/pages/subscriptions/subscriptions.page.resolver.ts
@@ -4,8 +4,9 @@ import { Store, Select } from '@ngxs/store';
 import { Observable, of } from 'rxjs';
 import { switchMap, take } from 'rxjs/operators';
 
-import { StateUserSubscriptions, ActionUserWatchSubscriptionsStatus } from '@firefly/core';
+import { StateUserSubscriptions, ActionUserWatchSubscriptionsStatus, StateUser, ActionUserSubscriptionsSetData } from '@firefly/core';
 import { ActionMobileLoadingShow, ActionMobileLoadingHide } from '@firefly/mobile';
+import { SubscriptionPartial } from '../../../../../cloud/src/library/models';
 
 @Injectable({ providedIn: 'root' })
 export class ResolverPageSubscriptions implements Resolve<void>
@@ -16,8 +17,8 @@ export class ResolverPageSubscriptions implements Resolve<void>
 
     public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>
     {
-      //return of(null);
-        //return this.store.dispatch(new ActionUserWatchSubscriptionsStatus())
+        const subscriptions: Record<string, SubscriptionPartial> = this.store.selectSnapshot(StateUser.subscriptionsStatus);
+
         return this.initialized$.
         pipe
         (
@@ -28,7 +29,7 @@ export class ResolverPageSubscriptions implements Resolve<void>
                     this.store.dispatch(new ActionMobileLoadingShow()).
                     pipe
                     (
-                        switchMap(() => this.store.dispatch(new ActionUserWatchSubscriptionsStatus())),
+                        switchMap(() => this.store.dispatch(new ActionUserSubscriptionsSetData(subscriptions, true))),
                         switchMap(() => this.store.dispatch(new ActionMobileLoadingHide()))
                     )
             )
