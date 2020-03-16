94b7d32e6f00cf153e7a91003e2e63527719b8ea
davidbeaton88 <david.beaton88@gmail.com>
2020-03-16 01:15:22 -0400

    added interest-detail

---
+++ apps/firefly/mobile/src/app/pages/interest-detail/index.ts
@@ -0,0 +1,4 @@
+export * from './interest-detail.page.module';
+export * from './interest-detail.page.resolver';
+export * from './interest-detail.page.routes';
+export * from './interest-detail.page';
---
+++ apps/firefly/mobile/src/app/pages/interest-detail/interest-detail.page.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { ModuleComponentItemHeader, ModuleComponentItemDescription, ModuleComponentItemMap } from '@firefly/mobile';
+import { ModulePage } from '@firefly/app/modules';
+
+import { PageInterestDetail } from './interest-detail.page';
+import { RoutesPageInterestDetail } from './interest-detail.page.routes';
+import { ModulePageIconSelector } from '../icon-selector';
+import { NgxsFormPluginModule } from '@ngxs/form-plugin';
+import { ModuleComponentButtonAdd } from '@firefly/core';
+import { ModulePageEventSelector } from '../event-selector';
+@NgModule
+({
+    imports :
+    [
+        ReactiveFormsModule,
+        RouterModule,
+        ModulePage,
+        ModuleComponentItemHeader,
+        ModuleComponentItemMap,
+        ModuleComponentItemDescription,
+        ModuleComponentButtonAdd,
+        RouterModule.forChild(RoutesPageInterestDetail),
+        ModulePageIconSelector,
+        ModulePageEventSelector,
+        NgxsFormPluginModule
+    ],
+
+    declarations : [PageInterestDetail],
+    exports : [PageInterestDetail],
+    entryComponents:
+    [
+      PageInterestDetail
+    ]
+})
+
+export class ModulePageInterestDetail { }
