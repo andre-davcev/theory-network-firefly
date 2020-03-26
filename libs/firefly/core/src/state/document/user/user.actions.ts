7c07cd8833a4b2f83f8e0b3620c13dc3c074a154
Andre Davcev <>
2020-03-26 00:27:39 -0400

    90% implement of interest type filter

--- libs/firefly/core/src/state/document/user/user.actions.ts
+++ libs/firefly/core/src/state/document/user/user.actions.ts
@@ -3,6 +3,7 @@ import { Credentials } from '@theory/core';
 
 import { User } from '@firefly/cloud';
 import { ActionsUser } from './user.actions.enum';
+import { InterestType } from '@firefly/core/enums';
 
 export class ActionUserReset  { static readonly type = ActionsUser.Reset;   constructor() { } }
 export class ActionUserGet    { static readonly type = ActionsUser.Get;     constructor(public id: string) { } }
@@ -29,3 +30,5 @@ export class ActionUserSubscriptionToggle       { static readonly type = Actions
 export class ActionUserSubscriptionAdd          { static readonly type = ActionsUser.SubscriptionAdd;          constructor(public id: string) { } }
 export class ActionUserSubscriptionRemove       { static readonly type = ActionsUser.SubscriptionRemove;       constructor(public id: string) { } }
 export class ActionUserSubscriptionOnOff        { static readonly type = ActionsUser.SubscriptionOnOff;        constructor(public id: string, public on: boolean) { } }
+
+export class ActionUserInterestTypeSet { static readonly type = ActionsUser.InterestTypeSet; constructor(public interestType: InterestType) { } }
--- libs/firefly/core/src/state/query/user-interests/user-interests.state.ts
+++ libs/firefly/core/src/state/query/user-interests/user-interests.state.ts
@@ -1,6 +1,6 @@
-import { State, Action, StateContext, Store } from '@ngxs/store';
+import { State, Action, StateContext, Store, Selector } from '@ngxs/store';
 
-import { Interest } from '@firefly/cloud';
+import { Interest, StreamInterest, SubscriptionPartial } from '@firefly/cloud';
 import { ServiceInterests } from '@firefly/core/services';
 import { StateQuery } from '@theory/ngxs';
 
@@ -17,6 +17,8 @@ import {
 import { StateUser } from '../../document/user/user.state';
 import { Query } from '@angular/fire/firestore';
 import { Injectable } from '@angular/core';
+import { StateUserStream } from '../../child/user-stream/user-stream.state';
+import { InterestType } from '@firefly/core/enums';
 
 @State<StateUserInterestsModel>(StateUserInterestsOptions)
 @Injectable()
@@ -40,6 +42,56 @@ export class StateUserInterests extends StateQuery<Interest, StateUserInterestsM
                 ActionSync    : ActionUserInterestsSync
             }
         );
+    }
+
+    @Selector([StateUserStream.data(), StateUser.subscriptionsStatus, StateUser.interestType])
+    public static stream
+    (
+        state         : StateUserInterestsModel,
+        stream        : Array<StreamInterest>,
+        subscriptions : Record<string, SubscriptionPartial>,
+        interestType  : InterestType
+    ) : Array<StreamInterest>
+    {
+        return interestType === InterestType.Created ?
+            StateUserInterests.
+                dataState(state).
+                map((interest: Interest) => {
+                    return {
+                        ...interest,
+                        score : 0,
+                        on    : subscriptions[interest.id] == null ? false : subscriptions[interest.id].on
+                    };
+                }) :
+            stream.
+                filter((interest: StreamInterest) =>
+                    (interestType === InterestType.Subscribed && subscriptions[interest.id] != null) ||
+                    (interestType === InterestType.Unsubscribed && (subscriptions[interest.id] == null || interest.on != null))
+                );
+    }
+
+    @Selector([StateUserStream.data(), StateUser.subscriptionsStatus, StateUser.interestType])
+    public static streamFound
+    (
+        state         : StateUserInterestsModel,
+        stream        : Array<StreamInterest>,
+        subscriptions : Record<string, SubscriptionPartial>,
+        interestType  : InterestType
+    ): boolean
+    {
+        return StateUserInterests.stream(state, stream, subscriptions, interestType).length > 0;
+    }
+
+    @Selector([StateUserStream.data(), StateUser.subscriptionsStatus, StateUser.interestType])
+    public static streamEmpty
+    (
+        state         : StateUserInterestsModel,
+        stream        : Array<StreamInterest>,
+        subscriptions : Record<string, SubscriptionPartial>,
+        interestType  : InterestType
+    ): boolean
+    {
+        return !StateUserInterests.streamFound(state, stream, subscriptions, interestType);
     }
 
     @Action(ActionUserInterestsReset)
