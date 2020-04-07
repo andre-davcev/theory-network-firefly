4b7b19c9811d7a0ae59b119d37f7d21305274636
Andre Davcev <>
2020-04-06 22:21:46 -0400

    Cleanup pages and move to unused

--- apps/firefly/mobile/src/app/pages/assets-places/assets-places.page.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { PageAssetsPlaces } from './assets-places.page';
-
-export const RoutesPageAssetsPlaces: Routes =
-[
-    { path : '', component : PageAssetsPlaces }
-];
