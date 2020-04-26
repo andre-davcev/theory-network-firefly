9c6ff31ef907172286a7b1fc62d27b8f0908738a
Andre Davcev <>
2020-04-26 15:09:22 -0400

    Minor removal of bucketPath references

--- apps/firefly/cloud/src/users/create.function.ts
+++ apps/firefly/cloud/src/users/create.function.ts
@@ -24,7 +24,6 @@ onCreate(async (snapshot: DocumentSnapshot, context: EventContext) =>
 
         name        : 'Your first alert!',
         description : `This is your first alert. Once you subscribe to Firefly Interests found on the home Discover screen, you will receive alerts when new events are posted in each interest!`,
-        bucketPath  : 'baLysAd71cRyZjh0hr6poxR8an13/images/nA8wxI4UIhEU0YjfwWPe.jpeg',
         interests   : [],
         timeStart   : new Date().getMilliseconds().toString(),
         read        : false,
