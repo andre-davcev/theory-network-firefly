e2b79aca6da9042d5983fe8786cd8fc567758875
davidbeaton88 <david.beaton88@gmail.com>
2020-05-24 03:25:45 -0400

    added event update/create functions to create/update index in algolia

--- apps/firefly/cloud/src/events/create.function.ts
+++ apps/firefly/cloud/src/events/create.function.ts
@@ -2,6 +2,13 @@ import { firestore, EventContext, CloudFunction } from 'firebase-functions';
 import { DocumentSnapshot, Firestore } from '@google-cloud/firestore';
 import { firestore as db } from 'firebase-admin';
 import { ServiceFirestore, Version, ServiceCities, Event, Collection } from '../library';
+import algoliasearch from 'algoliasearch';
+import * as functions from 'firebase-functions';
+
+const env = functions.config();
+
+const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
+const index = client.initIndex('events');
 
 const database: Firestore = db();
 
@@ -12,11 +19,14 @@ document(`${Collection.Events}/{id}`).
 onCreate(async(snapshot: DocumentSnapshot, context: EventContext) =>
 {
     const object: Event = ServiceFirestore.create<Event>(snapshot, Version.Events);
+    const data = snapshot.data();
+    const objectID = snapshot.id;
 
     return Promise.all
     ([
         snapshot.ref.update(object),
-        ServiceCities.createIfNew(database, object)
+        ServiceCities.createIfNew(database, object),
+        index.saveObject({ ...data, objectID})
     ]);
 });
 
