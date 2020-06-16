e741673fcf07124c0f8f77d700b72d826c8d780a
Andre Davcev <>
2020-06-16 01:05:37 -0400

    - Hide events in the past - Order events by date ascending - Group dates together

--- libs/firefly/core/src/state/child/user-alerts/user-alerts.state.options.ts
+++ libs/firefly/core/src/state/child/user-alerts/user-alerts.state.options.ts
@@ -3,7 +3,7 @@ import { StoreOptions } from '@ngxs/store/src/symbols';
 import { PageSize } from '@theory/ngxs';
 
 import { StateUserAlertsModel } from './user-alerts.state.model';
-import { OrderBy, ImageSize } from '@theory/firebase';
+import { OrderBy } from '@theory/firebase';
 import { TypeOf } from '@theory/core';
 
 export const StateUserAlertsOptions: StoreOptions<StateUserAlertsModel> =
@@ -14,7 +14,7 @@ export const StateUserAlertsOptions: StoreOptions<StateUserAlertsModel> =
     {
         pageSize:         PageSize.Default,
         orderBy:          'timeStart',
-        orderByDirection: OrderBy.Descending,
+        orderByDirection: OrderBy.Ascending,
 
         initialized:    false,
         loading:        false,
