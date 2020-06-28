fadedc362ed302cc71188488c48918823617c22c
davidbeaton88 <david.beaton88@gmail.com>
2020-06-28 19:28:10 -0400

    fixed edit event issue

--- apps/firefly/mobile/src/app/pages/asset-event/asset-event.page.resolver.ts
+++ apps/firefly/mobile/src/app/pages/asset-event/asset-event.page.resolver.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { ActionEventSetId } from '@firefly/core';
 import { switchMap } from 'rxjs/operators';
 import { ActionMobileLoadingHide } from '@firefly/mobile';
+import { CoreEnum } from '@theory/core';
 
 @Injectable({ providedIn: 'root' })
 export class ResolverPageAssetEvent implements Resolve<void>
@@ -14,10 +15,16 @@ export class ResolverPageAssetEvent implements Resolve<void>
 
     public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<void>
     {
-        return this.store.dispatch(new ActionEventSetId(route.params.id)).
-        pipe
-        (
-            switchMap(() => this.store.dispatch(new ActionMobileLoadingHide()))
-        );
+        if(route.queryParams.id == CoreEnum.IdNew)
+        {
+          return this.store.dispatch(new ActionEventSetId(route.params.id)).
+          pipe
+          (
+              switchMap(() => this.store.dispatch(new ActionMobileLoadingHide()))
+          );
+        }
+        else{
+          this.store.dispatch(new ActionMobileLoadingHide());
+        }
     }
 }
