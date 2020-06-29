408697fa4a3f2ec95893947fa59f02559c40ec6c
Andre Davcev <>
2020-06-29 00:05:59 -0400

    Fix create event resolvers

--- apps/firefly/mobile/src/app/pages/asset-event/asset-event.page.resolver.ts
+++ apps/firefly/mobile/src/app/pages/asset-event/asset-event.page.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { ActionEventSetId } from '@firefly/core';
 import { switchMap } from 'rxjs/operators';
@@ -15,16 +15,16 @@ export class ResolverPageAssetEvent implements Resolve<void>
 
     public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<void>
     {
-        if(route.queryParams.id == CoreEnum.IdNew)
-        {
-          return this.store.dispatch(new ActionEventSetId(route.params.id)).
-          pipe
-          (
-              switchMap(() => this.store.dispatch(new ActionMobileLoadingHide()))
-          );
-        }
-        else{
-          this.store.dispatch(new ActionMobileLoadingHide());
-        }
+        const setId$: Observable<any> = route.params.id == CoreEnum.IdNew ?
+            this.store.dispatch(new ActionEventSetId(route.params.id)) :
+            of(null);
+
+        return setId$.
+        pipe
+        (
+            switchMap(() =>
+                this.store.dispatch(new ActionMobileLoadingHide())
+            )
+        );
     }
 }
