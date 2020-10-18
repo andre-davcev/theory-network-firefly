a3b5cc75418db476ce30f0a5c82835c7a40014aa
Andre Davcev <>
2020-10-18 18:47:32 -0400

    Pages cleanup

--- libs/firefly/mobile/src/enums/pages.enum.ts
+++ libs/firefly/mobile/src/enums/pages.enum.ts
@@ -1,58 +1,29 @@
 export enum Pages
 {
-    Auth   = 'auth',
+    Alert             = 'alerts',
+    AlertDetail       = 'alert-detail',
+    AssetEvent        = 'asset-event',
+    AssetInterest     = 'asset-interest',
+    AssetsEvents      = 'assets-events',
+    AssetsInterests   = 'assets-interests',
+    Auth              = 'auth',
+    Calendar          = 'calendar',
+    EventAssets       = 'event-assets',
+    EventDetail       = 'event-detail',
+    EventLibrary      = 'event-library',
+    EventLocation     = 'event-location',
+    Home              = 'home',
+    InterestDetail    = 'interest-detail',
+    Login             = 'login',
+    Stream            = 'stream',
+    UserProfile       = 'user-profile',
 
-    Login            = 'login',
-    SignUp           = 'sign-up',
-    ResetPassword    = 'reset-password',
-    SignUpCategories = 'sign-up-categories',
-    SignUpSlides     = 'sign-up-slides',
+    SignUp            = 'sign-up',
+    ResetPassword     = 'reset-password',
+    SignUpSlides      = 'sign-up-slides',
 
-    Home        = 'home',
-    Calendar    = 'calendar',
-    Stream      = 'stream',
-    Alert       = 'alerts',
-    AlertDetail = 'alert-detail',
+    IconSelector      = 'icon-selector',
+    EventSelector     = 'event-selector',
 
-    Search        = 'search',
-    Subscriptions = 'subscriptions',
-    Publisher     = 'publisher',
-    User          = 'user',
-
-    AssetInterest = 'asset-interest',
-    AssetEvent   = 'asset-event',
-    AssetPlace   = 'asset-image',
-    AssetIcon    = 'asset-icon',
-    AssetImage   = 'asset-image',
-
-    AssetsIcons    = 'assets-icons',
-    AssetsImages   = 'assets-images',
-    AssetsCoupons  = 'assets-coupons',
-    AssetsBeacons  = 'assets-beacons',
-    AssetsPlaces   = 'assets-places',
-    AssetsEvents   = 'assets-events',
-    AssetsInterests = 'assets-interests',
-
-    EventLocation = 'event-location',
-
-    EventDetail   = 'event-detail',
-
-    InterestDetail = 'interest-detail',
-
-    UserProfile  = 'user-profile',
-    UserSettings = 'user-settings',
-
-    IconSelector = 'icon-selector',
-    IconCatalog  = 'icon-catalog',
-    IconAssets   = 'icon-assets',
-    IconLibrary  = 'icon-library',
-
-    EventSelector = 'event-selector',
-    EventLibrary  = 'event-library',
-    EventAssets   = 'event-assets',
-
-    ImageSelector = 'image-selector',
-    ImageCatalog  = 'image-catalog',
-    ImageAssets   = 'image-assets',
-    ImageLibrary  = 'image-library'
+    ImageSelector     = 'image-selector'
 }
