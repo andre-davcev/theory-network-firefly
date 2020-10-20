2869eb296592da2a4030d6af557a0e62ed0ce8c3
Andre Davcev <>
2020-10-19 21:06:07 -0400

    Add “Open Website” To Go

--- apps/firefly/mobile/src/app/pages/notifications/notifications.page.resolver.ts
+++ apps/firefly/mobile/src/app/pages/notifications/notifications.page.resolver.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { ActionAlertSetId } from '@firefly/core';
+import { ActionEventSetId } from '@firefly/core';
 
 @Injectable({ providedIn: 'root' })
 export class ResolverPageNotifications implements Resolve<void>
@@ -11,6 +11,6 @@ export class ResolverPageNotifications implements Resolve<void>
 
     public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<void>
     {
-        return this.store.dispatch(new ActionAlertSetId(route.params.id));
+        return this.store.dispatch(new ActionEventSetId(route.params.id));
     }
 }
