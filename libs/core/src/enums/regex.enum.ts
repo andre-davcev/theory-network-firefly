09a14d1cd05378232016356b8a454f12888120d1
Andre Davcev <>
2020-10-27 12:00:14 -0400

    Add Phone/Website Validator

---
+++ libs/core/src/enums/regex.enum.ts
@@ -0,0 +1,9 @@
+export enum Regex
+{
+    Numbers = '^[0-9]*$',
+    Email   = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$',
+
+    WebsiteSecure = '^(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?',
+
+    PasswordUpperLowerNumbers = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'
+}
