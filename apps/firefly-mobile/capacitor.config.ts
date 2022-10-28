a92ac98488bf5471bd2a182e5751af68e7a1a850
Andre Davcev <>
2022-10-28 16:27:14 -0400

    Move capacitor.config.json to capacitor.config.ts

---
+++ apps/firefly-mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.theorynetwork.Firefly',
+  appName: 'Firefly',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
