396858f49ba8aa448b2a855040495f4508b63cfa
Andre Davcev <>
2022-11-04 11:02:12 -0400

    Fix map.component.ts and add MapboxGeocoder

--- libs/firefly-shared/src/components/map/map.component.ts
+++ libs/firefly-shared/src/components/map/map.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit, Input, HostBinding, AfterViewInit, Output, EventEmitter, ViewChildren, QueryList, OnChanges, SimpleChanges } from '@angular/core';
-import { Select } from '@ngxs/store';
+import { Component, OnInit, Input, HostBinding, AfterViewInit, Output, EventEmitter, ViewChildren, QueryList, OnChanges, SimpleChanges, Inject, NgZone } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { filter, takeUntil, delay, switchMap, map } from 'rxjs/operators';
-import { LngLatLike, Point, Popup } from 'mapbox-gl';
-import { LngLatLiteral } from '@mapbox/mapbox-gl-geocoder';
+import { LngLatLike, Point, Popup, Map } from 'mapbox-gl';
 import { MapComponent, MarkerComponent } from 'ngx-mapbox-gl';
-import { Results, Result } from '@mapbox/mapbox-gl-geocoder';
+import { Results, Result, MapboxGeocoder, LngLatLiteral } from '@mapbox/mapbox-gl-geocoder';
 
 import { StateDevice, StateLanguage, StateLocation } from '@theory/capacitor';
 import { BaseComponent } from '@theory/core';
-import { MapMovingMethod } from '@theory/mapbox';
+import { EnvironmentMapbox, MapboxEnvironment, MapMovingMethod } from '@theory/mapbox';
 import { MapboxPlaceType, MapboxMapStyle, MapboxControlPosition, MapboxMarkerAnchor } from '@theory/mapbox';
 import { Place } from '@firefly/cloud';
 
@@ -43,6 +42,7 @@ export class ComponentMap extends BaseComponent implements OnInit, AfterViewInit
     public mapReady$: Observable<boolean>;
     private contentInitiated$: BehaviorSubject<boolean> = new BehaviorSubject(false);
     public Color: any = Color;
+    private geocoder: MapboxGeocoder;
 
     @ViewChildren('map')    maps:    QueryList<MapComponent>;
     @ViewChildren('marker') markers: QueryList<MarkerComponent>;
@@ -57,7 +57,6 @@ export class ComponentMap extends BaseComponent implements OnInit, AfterViewInit
     @HostBinding('style.height')
     public height: string = '100%';
 
-    @Input() searchInput:  string  = '';
     @Input() placeholder?: string  = 'Search';
     @Input() limit:        number  = 5;
     @Input() flyTo:        boolean = true;
@@ -87,9 +86,13 @@ export class ComponentMap extends BaseComponent implements OnInit, AfterViewInit
     @Output() searchResult  : EventEmitter<Result>  = new EventEmitter<Result>();
     @Output() searchError   : EventEmitter<any>     = new EventEmitter<any>();
 
-    private map: mapboxgl.Map;
+    private map: Map;
 
-    constructor()
+    constructor(
+      private store: Store,
+      private zone: NgZone,
+      @Inject(MapboxEnvironment) private environment: EnvironmentMapbox
+    )
     {
         super();
     }
@@ -126,8 +129,23 @@ export class ComponentMap extends BaseComponent implements OnInit, AfterViewInit
     }
 
 
-    public onLoad(map: mapboxgl.Map): void
+    public onLoad(map: Map): void
     {
+        if (this.geocode)
+        {
+            this.geocoder = this.createGeocoder();
+
+            map.addControl(this.geocoder);
+
+            this.language$.pipe(takeUntil(this.destroy$)).subscribe((language: string) =>
+              this.geocoder.setLanguage(language)
+            );
+
+            this.locationLiteral$.pipe(takeUntil(this.destroy$)).subscribe((proximity: LngLatLiteral) =>
+              this.geocoder.setProximity(proximity)
+            );
+        }
+
         this.map = map;
 
         this.resizeMap();
@@ -198,5 +216,37 @@ export class ComponentMap extends BaseComponent implements OnInit, AfterViewInit
         {
             this.map.resize();
         }
+    }
+
+    private createGeocoder(): MapboxGeocoder
+    {
+        const language: string = this.store.selectSnapshot(StateLanguage.languageIso639_1);
+        const proximity: LngLatLiteral = this.store.selectSnapshot(StateLocation.locationLiteral);
+
+        const geocoder: MapboxGeocoder = new MapboxGeocoder({
+          accessToken: this.environment.accessToken,
+          position: this.geocodePosition,
+          placeholder: this.placeholder,
+          limit: this.limit,
+          flyTo: this.flyTo,
+          bbox: this.bbox,
+          zoom: this.zoom,
+          types: this.types.join(','),
+          minLength: this.minLength,
+          language,
+          proximity
+        });
+
+        geocoder.
+          on('results', (event: MapboxGeocoder.Results) => this.zone.run(() => this.clear.emit(event))).
+          on('result', (event: { result: MapboxGeocoder.Result }) => this.zone.run(() => this.searchResult.emit(event))).
+          on('error', (event: any) => this.zone.run(() => this.searchError.emit(event))).
+          on('loading', (event: { query: string }) => this.zone.run(() => this.loading.emit(event.query))).
+          on('clear', () => this.zone.run(() => this.clear.emit()));
+
+        // [language]="language$ | async"
+        // [proximity]="locationLiteral$ | async"
+
+        return geocoder;
     }
 }
