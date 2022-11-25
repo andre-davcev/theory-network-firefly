3ce30cf1aeb42c1798456abf8f3231e81fe9f582
Andre Davcev <>
2022-11-25 10:08:17 -0500

    Add parallax scroll

--- apps/firefly-web/src/app/app.component.ts
+++ apps/firefly-web/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'theory-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
