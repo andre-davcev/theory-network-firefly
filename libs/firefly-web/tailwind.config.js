5d1a1df47f65f26943ced217d1a2fd32e42e7751
Andre Davcev <>
2023-05-08 01:59:19 -0400

    Fix tailwind config missing

---
+++ libs/firefly-web/tailwind.config.js
@@ -0,0 +1,66 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#9AE449',
+          DEFAULT: '#80D623',
+          dark: '#61B10B',
+        },
+        secondary: {
+          light: '#DB477E',
+          DEFAULT: '#CE2262',
+          dark: '#AA0B46',
+        },
+        white: '#ffffff',
+        black: '#000000',
+      },
+      fontFamily: {
+        sans: [
+          'Source Sans Pro',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        serif: [
+          'ui-serif',
+          'Georgia',
+          'Cambria',
+          '"Times New Roman"',
+          'Times',
+          'serif'
+        ],
+        mono: [
+          'ui-monospace',
+          'SFMono-Regular',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Liberation Mono"',
+          '"Courier New"',
+          'monospace',
+        ],
+      }
+    },
+  },
+  plugins: [],
+};
