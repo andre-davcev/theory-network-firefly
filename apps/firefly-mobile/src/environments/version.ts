45d8396f75091be8821ee72307db1e21d9e5d73b
Andre Davcev <>
2023-08-30 00:33:17 -0400

    - Fix text inputs not scrolling correctly when at bottom of page by installing @capacitor/keyboard

--- apps/firefly-mobile/src/environments/version.ts
+++ apps/firefly-mobile/src/environments/version.ts
@@ -1,14 +1,14 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  dirty: true,
-  raw: '8ff4118b-dirty',
-  hash: '8ff4118b',
-  distance: null,
-  tag: null,
-  semver: null,
-  suffix: '8ff4118b-dirty',
-  semverString: null,
-  version: '1.0.0'
+    "dirty": true,
+    "raw": "513cc471-dirty",
+    "hash": "513cc471",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "513cc471-dirty",
+    "semverString": null,
+    "version": "1.0.0"
 };
 /* tslint:enable */
