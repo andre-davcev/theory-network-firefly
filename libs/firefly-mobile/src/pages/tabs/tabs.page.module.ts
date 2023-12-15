fa5f67b0771c39432247ed59d0c0af8050e0cd00
Andre Davcev <>
2023-12-15 01:01:15 -0500

    Add tabs -1 elevation

--- libs/firefly-mobile/src/pages/tabs/tabs.page.module.ts
+++ libs/firefly-mobile/src/pages/tabs/tabs.page.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { ModuleDirectiveElevation } from '@theory/google';
 import { ModulePage } from '../../modules';
 import { PageTabs } from './tabs.page';
 import { RoutesPageTabs } from './tabs.page.routes';
 
 @NgModule({
-  imports: [ModulePage, RouterModule.forChild(RoutesPageTabs)],
+  imports: [
+    ModulePage,
+    ModuleDirectiveElevation,
+    RouterModule.forChild(RoutesPageTabs)
+  ],
 
   declarations: [PageTabs],
   exports: [PageTabs]
